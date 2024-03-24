f80c7f7b82590fd7f3ab8d9a070151fa981b7ba5
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-03-23 20:44:28 -0600

    feat(lualine): Added settings, improved existing

--- lua/lazy_cfg/lualine/init.lua
+++ lua/lazy_cfg/lualine/init.lua
@@ -11,9 +11,9 @@ local Lualine = require('lualine')
 
 return Lualine.setup({
   	options = {
-      	icons_enabled = true,
+      	icons_enabled = false,
         theme = 'tokyonight',
-        component_separators = { left = '', right = ''},
+        component_separators = { left = '|', right = '|'},
         section_separators = { left = '', right = ''},
         disabled_filetypes = {
             statusline = {},
@@ -21,28 +21,76 @@ return Lualine.setup({
         },
         ignore_focus = {},
         always_divide_middle = true,
-        globalstatus = false,
+        globalstatus = true,
         refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-        }
+            statusline = 500,
+            tabline = 500,
+            winbar = 500,
+        },
     },
     sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
-        lualine_y = {'progress'},
+        lualine_a = {
+			{
+				'mode',
+				---@param str string
+				---@return string
+				fmt = function(str)
+					return str:sub(1,1)
+				end,
+			},
+        },
+        lualine_b = {
+        	'branch',
+        	-- 'diff',
+        	{
+        		'diagnostics',
+        		sources = { 'nvim_lsp' },
+        		sections = { 'error', 'warn' },
+        		symbols = {
+        			error = 'E',
+        			warn = 'W',
+					info = 'I',
+					hint = '?'
+				},
+        	},
+        },
+        lualine_c = {
+        	{
+        		'filename',
+        		file_status = true,
+        		newfile_status = true,
+        		path = 1,
+        		shorting_target = 15,
+        		symbold = {
+					modified = '[+]',
+					readonly = '[RO]',
+					unnamed = '[NONAME]',
+					newfile = '[NEW]'
+        		},
+        	},
+		},
+        lualine_x = {
+        	'encoding',
+        	'fileformat',
+			{
+				'filetype',
+				icon_only = false,
+				icon = { '', align = 'right' },
+			}
+		},
+        lualine_y = {
+        	'progress',
+        	-- { 'searchcount', timeout = 10 }
+        },
         lualine_z = {'location'}
     },
     inactive_sections = {
         lualine_a = {},
         lualine_b = {},
         lualine_c = {'filename'},
-        lualine_x = {'location'},
-        lualine_y = {},
-        lualine_z = {}
+        lualine_x = {},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
     },
     tabline = {},
     winbar = {},
