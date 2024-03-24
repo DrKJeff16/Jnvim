5941194e2935eb55e8c11cf9476731db800ba383
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-03-24 00:02:18 -0600

    fix(lazy): Formstting tweaks and added codeowners syntax.

--- lua/lazy_cfg/init.lua
+++ lua/lazy_cfg/init.lua
@@ -5,6 +5,7 @@ local fn = vim.fn
 local opt = vim.opt
 local let = vim.g
 local set = vim.o
+local api = vim.api
 
 local map = api.nvim_set_keymap
 
@@ -15,11 +16,11 @@ local exists = User.exists
 
 if not exists('lazy') or not vim.loop.fs_stat(lazypath) then
   	fn.system({
-    	"git",
-    	"clone",
-    	"--filter=blob:none",
-    	"https://github.com/folke/lazy.nvim.git",
-    	"--branch=stable", -- latest stable release
+    	'git',
+    	'clone',
+    	'--filter=blob:none',
+    	'https://github.com/folke/lazy.nvim.git',
+    	'--branch=stable', -- latest stable release
     	lazypath,
   	})
 end
@@ -50,33 +51,31 @@ Lazy.setup({
 	{ 'tpope/vim-fugitive', lazy = false },
 	{ 'tpope/vim-speeddating', lazy = true },
 
-	{ 'vim-scripts/L9', lazy = false },
+	{ 'vim-scripts/L9', lazy = false, priority = 1000 },
 
 	{
 		'nvim-treesitter/nvim-treesitter',
 		lazy = true,
 		build = ':verbose TSUpdate',
-		priority = 850,
 		dependencies = {
 			'nvim-treesitter/nvim-treesitter-context',
 			'nvim-orgmode/orgmode',
-			"JoosepAlviste/nvim-ts-context-commentstring",
+			'JoosepAlviste/nvim-ts-context-commentstring',
 		},
 	},
 	{ 'nvim-treesitter/nvim-treesitter-context', lazy = true },
 	{ 'nvim-orgmode/orgmode', lazy = true },
-	{ "JoosepAlviste/nvim-ts-context-commentstring", lazy = true },
+	{ 'JoosepAlviste/nvim-ts-context-commentstring', lazy = true },
 
 	{
 		'neovim/nvim-lspconfig',
 		lazy = true,
-		priority = 850,
 		dependencies = { 'folke/neodev.nvim' },
 	},
 	{ 'folke/neodev.nvim', lazy = true, dependencies = { 'folke/neoconf.nvim' } },
 	{ 'folke/neoconf.nvim', lazy = true },
 
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000, lazy = true },
+	{ 'catppuccin/nvim', name = 'catppuccin', priority = 1000, lazy = true },
 	{
 		'folke/tokyonight.nvim',
 		priority = 1000,
@@ -84,7 +83,7 @@ Lazy.setup({
 			local Tokyonight = require('tokyonight')
 
 			Tokyonight.setup({
-				style = "night",
+				style = 'night',
   				transparent = false,
   				terminal_colors = true,
   				styles = {
@@ -92,8 +91,8 @@ Lazy.setup({
   					keywords = { italic = false },
   					functions = { bold = true },
   					variables = {},
-  					sidebars = "dark",
-  					floats = "dark",
+  					sidebars = 'dark',
+  					floats = 'dark',
   				},
 			})
 
@@ -102,40 +101,39 @@ Lazy.setup({
 	},
 
 	{
-  		"folke/todo-comments.nvim",
-  		dependencies = { "nvim-lua/plenary.nvim" },
+  		'folke/todo-comments.nvim',
+  		dependencies = { 'nvim-lua/plenary.nvim' },
   		opts = {},
+  		priority = 1000,
 	},
+
 	{
 		'nvim-lua/plenary.nvim',
 		lazy = true,
 		cmd = {
-			"PlenaryBustedFile",
-			"PlenaryBustedDirectory",
+			'PlenaryBustedFile',
+			'PlenaryBustedDirectory',
 		},
+  		priority = 1000,
 	},
-
-	-- { "lewis6991/hover.nvim", lazy = true },
 
 	{
 		'hrsh7th/nvim-cmp',
 		lazy = true,
-		event = { 'InsertEnter', 'CmdlineEnter' },
-		priority = 1000,
 		dependencies = {
 			'nvim-treesitter/nvim-treesitter',
 			'neovim/nvim-lspconfig',
 			'onsails/lspkind.nvim',
 
-		    "hrsh7th/cmp-nvim-lsp",
-		    "hrsh7th/cmp-nvim-lua",
-		    "hrsh7th/cmp-nvim-lsp-document-symbol",
-		    "hrsh7th/cmp-nvim-lsp-signature-help",
-		    "hrsh7th/cmp-buffer",
-		    "hrsh7th/cmp-path",
-		    "petertriho/cmp-git",
-		    "FelipeLema/cmp-async-path",
-		    "hrsh7th/cmp-cmdline",
+		    'hrsh7th/cmp-nvim-lsp',
+		    'hrsh7th/cmp-nvim-lua',
+		    'hrsh7th/cmp-nvim-lsp-document-symbol',
+		    'hrsh7th/cmp-nvim-lsp-signature-help',
+		    'hrsh7th/cmp-buffer',
+		    'hrsh7th/cmp-path',
+		    'petertriho/cmp-git',
+		    'FelipeLema/cmp-async-path',
+		    'hrsh7th/cmp-cmdline',
 		    'saadparwaiz1/cmp_luasnip',
 		},
 	},
@@ -154,18 +152,27 @@ Lazy.setup({
 	{
 		'L3MON4D3/LuaSnip',
 		lazy = true,
-		dependencies = { "rafamadriz/friendly-snippets" },
+		dependencies = { 'rafamadriz/friendly-snippets' },
 		build = 'make install_jsregexp',
 	},
-	{ "rafamadriz/friendly-snippets", lazy = true },
+	{ 'rafamadriz/friendly-snippets', lazy = true },
 
 	{
-		"nvim-telescope/telescope.nvim",
+		'nvim-telescope/telescope.nvim',
     	lazy = true,
     	cmd = 'Telescope',
-    	dependencies = { "nvim-telescope/telescope-fzf-native.nvim" },
+    	dependencies = { 'nvim-telescope/telescope-fzf-native.nvim' },
+    	priority = 1000,
+    	enabled = false,
 	},
-	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make", lazy = true },
+	{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make', lazy = true, enabled = false },
+	{
+		'ahmedkhalf/project.nvim',
+		lazy = true,
+		cmd = 'Telescope projects',
+		priority = 300,
+		enabled = false,
+	},
 
 	{ 'nvim-tree/nvim-web-devicons', lazy = true },
 	{
@@ -185,14 +192,19 @@ Lazy.setup({
 	{ 'windwp/nvim-autopairs', lazy = true, enabled = false },
 
 	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
+		'lukas-reineke/indent-blankline.nvim',
+		main = 'ibl',
 		lazy = true,
 		dependencies = { 'https://gitlab.com/HiPhish/rainbow-delimiters.nvim' },
 		enabled = false,
 	},
+	{
+		'https://gitlab.com/HiPhish/rainbow-delimiters.nvim',
+		lazy = true,
+		enabled = false,
+	},
 
-	{ "ahmedkhalf/project.nvim", lazy = true, cmd = "Telescope projects" },
+	{ 'rhysd/vim-syntax-codeowners', lazy = false, priority = 300 },
 })
 
 source_cfg('lspconfig')
