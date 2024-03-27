e0fd7ecd6b8a0fe2a08d92fa4408a859451508ea
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-03-27 17:40:51 -0600

    fix(types): Added placeholder for submodule calling.

--- lua/user/types/init.lua
+++ lua/user/types/init.lua
@@ -1,3 +1,6 @@
+---@diagnostic disable:unused-local
+---@diagnostic disable:unused-function
+
 ---@alias VimOption
 ---|string
 ---|number
@@ -39,3 +42,46 @@
 ---@field multisrc? fun(self: UserMod, mods: ModTbl, prefix: string): any
 ---@field exists fun(mod: string): boolean
 ---@field assoc fun()
+
+local pfx = 'user.types.'
+
+---@param subs string[]
+---@param prefix? string
+---@return table<string, fun(): any> res
+local src = function(subs, prefix)
+	prefix = prefix or pfx
+
+	---@type table<string, fun(): any>
+	local res = {}
+
+	for _, v in next, subs do
+		local path = prefix..v
+		local ok, _ = pcall(require, path)
+		if ok then
+			---@return any
+			res[v] = function()
+				return require(path)
+			end
+		end
+	end
+
+	return res
+end
+
+---@type string[]
+local submods = {
+	'lspconfig',
+	'cmp',
+	'treesitter',
+	'opts',
+	'nvim_tree',
+	'todo_comments',
+}
+
+local subs = src(submods)
+
+if #subs > 0 then
+	for _, v in next, subs do
+		v()
+	end
+end
