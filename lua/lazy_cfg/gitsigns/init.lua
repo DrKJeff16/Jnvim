b00497f743e996f304b83d3c2a2d365218653935
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-04-01 21:53:06 -0600

    fix(gitsigns): Redid using the maps API, plus small tweaks.

--- lua/lazy_cfg/gitsigns/init.lua
+++ lua/lazy_cfg/gitsigns/init.lua
@@ -8,39 +8,38 @@ if not exists('gitsigns') then
 	return
 end
 
-local pfx = 'lazy_cfg.gitsigns.'
-
 local Gsig = require('gitsigns')
+
+local bmap = User.maps().buf_map
+local nmap = bmap.n
+local vmap = bmap.v
+local omap = bmap.o
+local xmap = bmap.x
 
 Gsig.setup({
 	on_attach = function(bufnr)
-		local function map(mode, lhs, rhs, opts)
-			opts = vim.tbl_extend('force', {noremap = true, silent = true}, opts or {})
-			vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
-		end
-
 		-- Navigation
-		map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", {expr=true})
-		map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", {expr=true})
+		nmap(']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", { expr = true })
+		nmap('[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", { expr = true })
 
 		-- Actions
-		map('n', '<leader>hs', ':Gitsigns stage_hunk<CR>')
-		map('v', '<leader>hs', ':Gitsigns stage_hunk<CR>')
-		map('n', '<leader>hr', ':Gitsigns reset_hunk<CR>')
-		map('v', '<leader>hr', ':Gitsigns reset_hunk<CR>')
-		map('n', '<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
-		map('n', '<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
-		map('n', '<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
-		map('n', '<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
-		map('n', '<leader>hb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
-		map('n', '<leader>tb', '<cmd>Gitsigns toggle_current_line_blame<CR>')
-		map('n', '<leader>hd', '<cmd>Gitsigns diffthis<CR>')
-		map('n', '<leader>hD', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
-		map('n', '<leader>td', '<cmd>Gitsigns toggle_deleted<CR>')
+		nmap('<leader>hs', ':Gitsigns stage_hunk<CR>')
+		vmap('<leader>hs', ':Gitsigns stage_hunk<CR>')
+		nmap('<leader>hr', ':Gitsigns reset_hunk<CR>')
+		vmap('<leader>hr', ':Gitsigns reset_hunk<CR>')
+		nmap('<leader>hS', '<cmd>Gitsigns stage_buffer<CR>')
+		nmap('<leader>hu', '<cmd>Gitsigns undo_stage_hunk<CR>')
+		nmap('<leader>hR', '<cmd>Gitsigns reset_buffer<CR>')
+		nmap('<leader>hp', '<cmd>Gitsigns preview_hunk<CR>')
+		nmap('<leader>hb', '<cmd>lua require"gitsigns".blame_line{full=true}<CR>')
+		nmap('<leader>tb', '<cmd>Gitsigns toggle_current_line_blame<CR>')
+		nmap('<leader>hd', '<cmd>Gitsigns diffthis<CR>')
+		nmap('<leader>hD', '<cmd>lua require"gitsigns".diffthis("~")<CR>')
+		nmap('<leader>td', '<cmd>Gitsigns toggle_deleted<CR>')
 
 		-- Text object
-		map('o', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-		map('x', 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+		omap('ih', ':<C-U>Gitsigns select_hunk<CR>')
+		xmap('ih', ':<C-U>Gitsigns select_hunk<CR>')
 	end,
 
 	signs = {
@@ -60,16 +59,16 @@ Gsig.setup({
 	attach_to_untracked = true,
 	current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
 	current_line_blame_opts = {
-		virt_text = true,
+		virt_text = false,
 		virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-		delay = 1000,
+		delay = 5000,
 		ignore_whitespace = false,
 		virt_text_priority = 100,
 	},
 	current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-	sign_priority = 6,
+	sign_priority = 4,
 	update_debounce = 100,
-	status_formatter = nil, -- Use default
+	-- status_formatter = nil, -- Use default
 	max_file_length = 40000, -- Disable if file is longer than this (in lines)
 	preview_config = {
 		border = 'single',
@@ -79,7 +78,5 @@ Gsig.setup({
 		col = 1,
 	},
 
-	yadm = {
-		enable = false,
-	},
+	yadm = { enable = false },
 })
