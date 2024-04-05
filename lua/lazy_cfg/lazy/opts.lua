10bb289002ae3d38608fbbb322c566964b8e9bb7
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-04-05 02:26:57 -0600

    feat(lazy): Added `opts` to customize.

--- lua/lazy_cfg/lazy/opts.lua
+++ lua/lazy_cfg/lazy/opts.lua
@@ -10,6 +10,7 @@ local g = vim.g
 
 local exists = fn.exists
 local has = fn.has
+local stdpath = fn.stdpath
 
 local Lazy = require('lazy')
 local Util = require('lazy.util')
@@ -17,9 +18,10 @@ local Util = require('lazy.util')
 ---@type LazyConfig
 local M = {
 	defaults = {
-		lazy = false,  -- WARN: DO NOT TOUCH.
+		lazy = false,  -- WARN: DO NOT SET TO TRUE.
 		version = '*',
 	},
+	install = { missing = true },
 	ui = {
 		size = {
 			width = 0.75,
@@ -27,16 +29,103 @@ local M = {
 		},
 		wrap = set.wrap,
 		border = exists('+termguicolors') == 1 and 'double' or 'none',
-		backdrop = 55,  -- Opacity
-		title = 'LAZY',
+		backdrop = 65,  -- Opacity
+		title = 'L   A   Z   Y',
 		title_pos = 'center',
 		pills = true,
 
 		browser = nil,
+		throttle = 15,
+
+		icons	= {
+			cmd = " ",
+			config = "",
+			event = " ",
+			ft = " ",
+			init = " ",
+			import = " ",
+			keys = " ",
+			lazy = "󰒲 ",
+			loaded = "●",
+			not_loaded = "○",
+			plugin = " ",
+			runtime = " ",
+			require = "󰢱 ",
+			source = " ",
+			start = " ",
+			task = "✔ ",
+			list = {
+				"●",
+				"➜",
+				"★",
+				"‒",
+			},
+		},
 
 		-- TODO: Define `<localleader>` mappings.
-		custom_keys = {},
+		custom_keys = {
+			['<localleader>t'] = {
+				function(plugin)
+					Util.float_term(nil, {
+						cwd = plugin.dir
+					})
+				end,
+				desc = 'Open Terminal In Plugin Dir',
+			},
+			['<localleader>'] = {
+				function(plugin)
+					Util.git_info(plugin.dir)
+				end,
+				desc = 'Retrieve a plugin\'s Git info.',
+			},
+		},
 	},
+
+	dkff = { cmd = 'git', },
+
+	checker = {
+		enabled = true,
+		notify = true,
+		frequency = 1800,
+		check_pinned = false,
+	},
+
+	change_detection = {
+		enabled = true,
+		notify = true,
+	},
+
+	performance = {
+		cache = { enabled = true },
+		reset_packpath = true,
+
+		rtp = {
+			reset = true,
+			paths = {},
+			disabled_plugins = {
+				-- 'gzip',
+				-- 'matchit',
+				-- 'matchparen',
+				'netrwPlugin',
+				-- 'tarPlugin',
+				-- 'tohtml',
+				'tutor',
+				-- 'zipPlugin',
+			},
+		},
+	},
+
+	readme = {
+		enabled = true,
+		root = stdpath('state')..'/lazy/readme',
+		files = { 'README.md', 'lua/**/README.md' },
+		skip_if_doc_exists = true,
+	},
+
+	state = stdpath('state')..'/lazy/state.json',
+	build = { warn_on_override = true },
+
+	profiling = { loader = true, require = true },
 }
 
 return M
