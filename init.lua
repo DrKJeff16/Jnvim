11c3cc2d564cac4c895a8cdb603660eb018c10ef
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-04-17 01:23:25 -0600

    refactor(init): Simplified keymap setting a little bit.

--- init.lua
+++ init.lua
@@ -39,70 +39,66 @@ local opts = User.opts
 
 --- Table of mappings for each mode `(normal|insert|visual|terminal)`.
 --- Each mode contains its respective mappings.
---- `map_tbl.[n|i|v|t].opts` is an **API** option table.
----@type ApiMapModeDicts
+--- `map_tbl.[n|i|v|t]['<YOUR_KEY>'].opts` is an **API** option table.
 local map_tbl = {
 	n = {
-		{ lhs = '<Leader>fn', rhs = ':edit ', opts = { silent = false } },
-		{ lhs = '<Leader>fs', rhs = '<CMD>w<CR>' },
-		{ lhs = '<Leader>fS', rhs = ':w ', opts = { silent = false } },
-		{ lhs = '<Leader>ff', rhs = ':ed ', opts = { silent = false } },
-		{ lhs = '<Leader>fvs', rhs = '<CMD>luafile $MYVIMRC<CR>' },
-		{ lhs = '<Leader>fvl', rhs = '<CMD>luafile %<CR>' },
-		{ lhs = '<Leader>fvv', rhs = '<CMD>so %<CR>' },
-		{ lhs = '<Leader>fvV', rhs = ':so ', opts = { silent = false } },
-		{ lhs = '<Leader>fvL', rhs = ':luafile ', opts = { silent = false } },
-		{ lhs = '<Leader>fve', rhs = '<CMD>tabnew $MYVIMRC<CR>', opts = { silent = false } },
+		['<Leader>fn'] = { rhs = ':edit ', opts = { silent = false } },
+		['<Leader>fs'] = { rhs = '<CMD>w<CR>' },
+		['<Leader>fS'] = { rhs = ':w ', opts = { silent = false } },
+		['<Leader>ff'] = { rhs = ':ed ', opts = { silent = false } },
+		['<Leader>fvs'] = { rhs = '<CMD>luafile $MYVIMRC<CR>' },
+		['<Leader>fvl'] = { rhs = '<CMD>luafile %<CR>' },
+		['<Leader>fvv'] = { rhs = '<CMD>so %<CR>' },
+		['<Leader>fvV'] = { rhs = ':so ', opts = { silent = false } },
+		['<Leader>fvL'] = { rhs = ':luafile ', opts = { silent = false } },
+		['<Leader>fve'] = { rhs = '<CMD>tabnew $MYVIMRC<CR>', opts = { silent = false } },
 
-		{ lhs = '<Leader>wss', rhs = '<CMD>split<CR>' },
-		{ lhs = '<Leader>wsv', rhs = '<CMD>vsplit<CR>' },
-		{ lhs = '<Leader>wsS', rhs = ':split ', opts = { silent = false } },
-		{ lhs = '<Leader>wsV', rhs = ':vsplit ', opts = { silent = false } },
+		['<Leader>wss'] = { rhs = '<CMD>split<CR>' },
+		['<Leader>wsv'] = { rhs = '<CMD>vsplit<CR>' },
+		['<Leader>wsS'] = { rhs = ':split ', opts = { silent = false } },
+		['<Leader>wsV'] = { rhs = ':vsplit ', opts = { silent = false } },
 
-		{ lhs = '<Leader>qq', rhs = '<CMD>qa<CR>' },
-		{ lhs = '<Leader>qQ', rhs = '<CMD>qa!<CR>' },
+		['<Leader>qq'] = { rhs = '<CMD>qa<CR>' },
+		['<Leader>qQ'] = { rhs = '<CMD>qa!<CR>' },
 
-		{ lhs = '<Leader>tn', rhs = '<CMD>tabN<CR>', opts = { silent = false } },
-		{ lhs = '<Leader>tp', rhs = '<CMD>tabp<CR>', opts = { silent = false } },
-		{ lhs = '<Leader>td', rhs = '<CMD>tabc<CR>' },
-		{ lhs = '<Leader>tD', rhs = '<CMD>tabc!<CR>' },
-		{ lhs = '<Leader>tf', rhs = '<CMD>tabfirst<CR>' },
-		{ lhs = '<Leader>tl', rhs = '<CMD>tablast<CR>' },
-		{ lhs = '<Leader>ta', rhs = ':tabnew ', opts = { silent = false } },
-		{ lhs = '<Leader>tA', rhs = '<CMD>tabnew<CR>' },
+		['<Leader>tn'] = { rhs = '<CMD>tabN<CR>', opts = { silent = false } },
+		['<Leader>tp'] = { rhs = '<CMD>tabp<CR>', opts = { silent = false } },
+		['<Leader>td'] = { rhs = '<CMD>tabc<CR>' },
+		['<Leader>tD'] = { rhs = '<CMD>tabc!<CR>' },
+		['<Leader>tf'] = { rhs = '<CMD>tabfirst<CR>' },
+		['<Leader>tl'] = { rhs = '<CMD>tablast<CR>' },
+		['<Leader>ta'] = { rhs = ':tabnew ', opts = { silent = false } },
+		['<Leader>tA'] = { rhs = '<CMD>tabnew<CR>' },
 
-		{ lhs = '<Leader>bn', rhs = '<CMD>bNext<CR>' },
-		{ lhs = '<Leader>bp', rhs = '<CMD>bprevious<CR>' },
-		{ lhs = '<Leader>bd', rhs = '<CMD>bdel<CR>' },
-		{ lhs = '<Leader>bD', rhs = '<CMD>bdel!<CR>' },
-		{ lhs = '<Leader>bf', rhs = '<CMD>bfirst<CR>' },
-		{ lhs = '<Leader>bl', rhs = '<CMD>blast<CR>' },
+		['<Leader>bn'] = { rhs = '<CMD>bNext<CR>' },
+		['<Leader>bp'] = { rhs = '<CMD>bprevious<CR>' },
+		['<Leader>bd'] = { rhs = '<CMD>bdel<CR>' },
+		['<Leader>bD'] = { rhs = '<CMD>bdel!<CR>' },
+		['<Leader>bf'] = { rhs = '<CMD>bfirst<CR>' },
+		['<Leader>bl'] = { rhs = '<CMD>blast<CR>' },
 
-		-- { lhs = '<Leader>fir', rhs = '<CMD>%retab<CR>' },
-
-		{ lhs = '<Leader>Ll', rhs = '<CMD>Lazy<CR>' },
-		{ lhs = '<Leader>LL', rhs = ':Lazy ', opts = { silent = false } },
-		{ lhs = '<Leader>Ls', rhs = '<CMD>Lazy sync<CR>' },
-		{ lhs = '<Leader>Lx', rhs = '<CMD>Lazy clean<CR>' },
-		{ lhs = '<Leader>Lc', rhs = '<CMD>Lazy check<CR>' },
-		{ lhs = '<Leader>Li', rhs = '<CMD>Lazy install<CR>' },
-		{ lhs = '<Leader>Lr', rhs = '<CMD>Lazy reload<CR>' },
-
+		['<Leader>Ll'] = { rhs = '<CMD>Lazy<CR>' },
+		['<Leader>LL'] = { rhs = ':Lazy ', opts = { silent = false } },
+		['<Leader>Ls'] = { rhs = '<CMD>Lazy sync<CR>' },
+		['<Leader>Lx'] = { rhs = '<CMD>Lazy clean<CR>' },
+		['<Leader>Lc'] = { rhs = '<CMD>Lazy check<CR>' },
+		['<Leader>Li'] = { rhs = '<CMD>Lazy install<CR>' },
+		['<Leader>Lr'] = { rhs = '<CMD>Lazy reload<CR>' },
 		-- Avoid entering visual while using `<leader>` sequences.
-		{ lhs = '<Leader>v', rhs = '<Nop>', opts = { nowait = false } },
+		['<Leader>v'] = { rhs = '<Nop>', opts = { nowait = false } },
 		-- Avoid entering insert while using `<leader>` sequences.
-		{ lhs = '<Leader>i', rhs = '<Nop>', opts = { nowait = false } },
-		{ lhs = '<Leader>"', rhs = '<Nop>', opts = { nowait = false } },
-		{ lhs = '<Leader>\'', rhs = '<Nop>', opts = { nowait = false } },
+		['<Leader>i'] = { rhs = '<Nop>', opts = { nowait = false } },
+		['<Leader>"'] = { rhs = '<Nop>', opts = { nowait = false } },
+		['<Leader>\''] = { rhs = '<Nop>', opts = { nowait = false } },
 	},
 	v = {
 		--- WARNING: DO NOT USE `<CMD>`!!!
-		{ lhs = '<Leader>is', rhs = ':sort<CR>' },
-		{ lhs = '<Leader>iS', rhs = ':sort!<CR>' },
+		['<Leader>is'] = { rhs = ':sort<CR>' },
+		['<Leader>iS'] = { rhs = ':sort!<CR>' },
 	},
 	t = {
 		-- Escape terminl by pressing `<Esc>`
-		{ lhs = '<Esc>', rhs = '<C-\\><C-n>' },
+		['<Esc>'] = { rhs = '<C-\\><C-n>' },
 	},
 }
 
@@ -111,9 +107,9 @@ for k, func in next, map do
 	local tbl = map_tbl[k]
 
 	-- If `tbl` exists, apply the mapping.
-	if tbl then
-		for _, v in next, tbl do
-			func(v.lhs, v.rhs, v.opts or {})
+	if tbl ~= nil and not vim.tbl_isempty(tbl) then
+		for lhs, v in next, tbl do
+			func(lhs, v.rhs, v.opts or {})
 		end
 	end
 end
