fab9983017d18b387a591d4c7f4f0aef1739f378
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-04-25 15:11:40 -0600

    chore(start): Tweaks.

--- lua/lazy_cfg/startup/init.lua
+++ lua/lazy_cfg/startup/init.lua
@@ -3,6 +3,7 @@
 
 local User = require('user')
 local Check = User.check
+
 local exists = Check.exists.module
 
 if not exists('startup') then
@@ -10,5 +11,7 @@ if not exists('startup') then
 end
 
 local Startup = require('startup')
+
+local opts = {}
 
 Startup.setup()
