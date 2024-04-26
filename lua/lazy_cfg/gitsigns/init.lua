6758ef0f394c647439720002c192829b658ce4a7
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-04-26 03:17:04 -0600

    fix(gitsigns): API integration, improvements.

--- lua/lazy_cfg/gitsigns/init.lua
+++ lua/lazy_cfg/gitsigns/init.lua
@@ -2,7 +2,13 @@
 ---@diagnostic disable:unused-function
 
 local User = require('user')
+local maps_t = User.types.user.maps
 local Check = User.check
+local bufmap = User.maps.buf_map
+
+local is_nil = Check.value.is_nil
+local is_num = Check.value.is_num
+local is_fun = Check.value.is_fun
 local exists = Check.exists.module
 local executable = Check.exists.executable
 
@@ -11,36 +17,36 @@ if not exists('gitsigns') or not executable('git') then
 end
 
 local types = User.types.gitsigns
-local bufmap = User.maps.buf_map
 
 local Gsig = require('gitsigns')
 
----@type UserBufModeKeys
+---@type table<string, BufMapTbl[]|BufMapArr[]>
 local keys = {
 	n = {
 		-- Navigati7on
-		{ lhs = '<leader>G]c', rhs = "&diff ? ']c' : '<CMD>Gitsigns next_hunk<CR>'", opts = { expr = true } },
-		{ lhs = '<leader>G[c', rhs = "&diff ? '[c' : '<CMD>Gitsigns prev_hunk<CR>'", opts = { expr = true } },
+		{ '<leader>G]c', "&diff ? ']c' : '<CMD>Gitsigns next_hunk<CR>'", { expr = true } },
+		{ '<leader>G[c', "&diff ? '[c' : '<CMD>Gitsigns prev_hunk<CR>'", { expr = true } },
 		-- Actions
-		{ lhs = '<leader>Ghs', rhs = ':Gitsigns stage_hunk<CR>' },
-		{ lhs = '<leader>Ghr', rhs = ':Gitsigns reset_hunk<CR>' },
-		{ lhs = '<leader>GhS', rhs = '<CMD>Gitsigns stage_buffer<CR>' },
-		{ lhs = '<leader>Ghu', rhs = '<CMD>Gitsigns undo_stage_hunk<CR>' },
-		{ lhs = '<leader>GhR', rhs = '<CMD>Gitsigns reset_buffer<CR>' },
-		{ lhs = '<leader>Ghp', rhs = '<CMD>Gitsigns preview_hunk<CR>' },
-		{ lhs = '<leader>Ghb', rhs = '<CMD>lua require"gitsigns".blame_line{full=true}<CR>' },
-		{ lhs = '<leader>Gtb', rhs = '<CMD>Gitsigns toggle_current_line_blame<CR>' },
-		{ lhs = '<leader>Ghd', rhs = '<CMD>Gitsigns diffthis<CR>' },
-		{ lhs = '<leader>GhD', rhs = '<CMD>lua require"gitsigns".diffthis("~")<CR>' },
-		{ lhs = '<leader>Gtd', rhs = '<CMD>Gitsigns toggle_deleted<CR>' },
+		{ '<leader>Ghs', ':Gitsigns stage_hunk<CR>' },
+		{ '<leader>Ghr', ':Gitsigns reset_hunk<CR>' },
+		{ '<leader>GhS', '<CMD>Gitsigns stage_buffer<CR>' },
+		{ '<leader>Ghu', '<CMD>Gitsigns undo_stage_hunk<CR>' },
+		{ '<leader>GhR', '<CMD>Gitsigns reset_buffer<CR>' },
+		{ '<leader>Ghp', '<CMD>Gitsigns preview_hunk<CR>' },
+		{ '<leader>Ghb', '<CMD>lua require"gitsigns".blame_line{full=true}<CR>' },
+		{ '<leader>Gtb', '<CMD>Gitsigns toggle_current_line_blame<CR>' },
+		{ '<leader>Ghd', '<CMD>Gitsigns diffthis<CR>' },
+		{ '<leader>GhD', '<CMD>lua require"gitsigns".diffthis("~")<CR>' },
+		{ '<leader>Gtd', '<CMD>Gitsigns toggle_deleted<CR>' },
 	},
 	v = {
-		{ lhs = '<leader>Ghs', rhs = ':Gitsigns stage_hunk<CR>' },
-		{ lhs = '<leader>Ghr', rhs = ':Gitsigns reset_hunk<CR>' },
+		{ '<leader>Ghs', ':Gitsigns stage_hunk<CR>' },
+		{ '<leader>Ghr', ':Gitsigns reset_hunk<CR>' },
 	},
 }
 
----@alias GitSignOpts { ['text']: string }
+---@class GitSignOpts
+---@field text string
 
 ---@class GitSigns
 ---@field add GitSignOpts
@@ -65,19 +71,27 @@ local signs = {
 local opts= {
 	---@param bufnr integer
 	on_attach = function(bufnr)
+		if not is_num(bufnr) or bufnr < 0 then
+			bufnr = 0
+		end
+
 		for mode, v in next, keys do
 			---@type BufMapFunction
 			local func = bufmap[mode]
 
 			for _, t in next, v do
-				func(bufnr, t.lhs, t.rhs, t.opts or {})
+				if not is_nil(t.lhs) and not is_nil(t.rhs) then
+					func(bufnr, t.lhs, t.rhs, t.opts or {})
+				elseif not is_nil(t[1]) and not is_nil(t[2]) then
+					func(bufnr, t[1], t[2], t[3] or {})
+				end
 			end
 		end
 	end,
 
 	signs = signs,
 
-	signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+	signcolumn = vim.o.signcolumn ==  'yes',  -- Toggle with `:Gitsigns toggle_signs`
 	numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
 	linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
 	word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
@@ -90,12 +104,11 @@ local opts= {
 		virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
 		delay = 5000,
 		ignore_whitespace = false,
-		virt_text_priority = 25,
+		virt_text_priority = 15,
 	},
 	current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-	sign_priority = 4,
+	sign_priority = 10,
 	update_debounce = 100,
-	-- status_formatter = nil, -- Use default
 	max_file_length = 40000, -- Disable if file is longer than this (in lines)
 	preview_config = {
 		border = 'double',
