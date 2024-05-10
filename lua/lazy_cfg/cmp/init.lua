6addc617693802adeb726282354ddad16a38d7ae
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-05-09 20:01:23 -0600

    fix(cmp): Various source fixes, code corrections.

--- lua/lazy_cfg/cmp/init.lua
+++ lua/lazy_cfg/cmp/init.lua
@@ -1,11 +1,9 @@
----@diagnostic disable:unused-local
----@diagnostic disable:unused-function
-
 local User = require('user')
 local Check = User.check
 local types = User.types.cmp
 
-local exists = User.check.exists.module
+local exists = Check.exists.module
+local is_fun = Check.value.is_fun
 
 local Types = require('cmp.types')
 local CmpTypes = require('cmp.types.cmp')
@@ -18,24 +16,16 @@ local bo = vim.bo
 local tbl_contains = vim.tbl_contains
 local get_mode = api.nvim_get_mode
 
----@return boolean
-local has_words_before = function()
-	unpack = unpack or table.unpack
-	local buf_lines = api.nvim_buf_get_lines
-	local win_cursor = api.nvim_win_get_cursor
-	local line, col = unpack(win_cursor(0))
-	return col ~= 0 and buf_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 local Luasnip = require('lazy_cfg.cmp.luasnip')
 local cmp = require('cmp')
-local sks = require('lazy_cfg.cmp.kinds')
+local Sks = require('lazy_cfg.cmp.kinds')
 local Util = require('lazy_cfg.cmp.util')
 
 local tab_map = Util.tab_map
 local s_tab_map = Util.s_tab_map
 local cr_map = Util.cr_map
 
+local has_words_before = Util.has_words_before
 local n_select = Util.n_select
 local n_shift_select = Util.n_shift_select
 local bs_map = Util.bs_map
@@ -50,17 +40,23 @@ cmp.setup({
 		local enable_comments = {
 			'bash',
 			'c',
+			'codeowners',
 			'cpp',
 			'css',
-			'gitcommit',
+			'gitattributes',
+			'gitconfig',
+			'gitignore',
 			'html',
 			'java',
 			'jsonc',
+			'less',
 			'lisp',
 			'lua',
 			'markdown',
 			'python',
+			'scss',
 			'sh',
+			'zsh',
 		}
 
 		if tbl_contains(enable_comments, ft) then
@@ -68,7 +64,9 @@ cmp.setup({
 		end
 
 		local disable_ft = {
+			'NvimTree',
 			'TelescopePrompt',
+			'lazy',
 		}
 
 		if tbl_contains(disable_ft, ft) then
@@ -93,25 +91,26 @@ cmp.setup({
 
 	completion = {
 		completeopt = 'menu,menuone,noselect,noinsert,preview',
+		keyword_length = 1,
 	},
 
-	view = sks.view,
-	formatting = sks.formatting,
-	window = sks.window,
+	view = Sks.view,
+	formatting = Sks.formatting,
+	window = Sks.window,
 
 	matching = {
-		disallow_fuzzy_matching = false,
 		disallow_fullfuzzy_matching = true,
-		disallow_prefix_unmatching = true,
-		disallow_partial_matching = false,
+		disallow_fuzzy_matching = false,
 		disallow_partial_fuzzy_matching = true,
+		disallow_partial_matching = false,
+		disallow_prefix_unmatching = true,
 		disallow_symbol_nonprefix_matching = true,
 	},
 
 	mapping = cmp.mapping.preset.insert({
 		['<C-j>'] = cmp.mapping.scroll_docs(-4),
 		['<C-k>'] = cmp.mapping.scroll_docs(4),
-		['<C-e>'] = cmp.mapping.abort(),
+		['<C-e>'] = cmp.mapping.abort(), -- Same as `<Esc>`
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<CR>'] = cmp.mapping(cr_map),
 		['<Tab>'] = cmp.mapping(tab_map),
@@ -124,28 +123,36 @@ cmp.setup({
 	}),
 
 	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
-		{ name = 'nvim_lsp_signature_help' },
-		{ name = 'luasnip' },
-	}, {
-		{ name = 'buffer' },
+		{ name = 'nvim_lsp',                priority = 1 },
+		{ name = 'luasnip',                 priority = 2 },
+		{ name = 'nvim_lsp_signature_help', priority = 3 },
+		{ name = 'buffer',                  priority = 4 },
 	}),
+})
+
+cmp.setup.filetype({ 'sh', 'bash', 'zsh' }, {
+	sources = cmp.config.sources({
+		{ name = 'nvim_lsp',                priority = 1 },
+		{ name = 'async_path',              priority = 2 },
+		{ name = 'luasnip',                 priority = 3 },
+		{ name = 'nvim_lsp_signature_help', priority = 4 },
+		{ name = 'buffer',                  priority = 5 },
+	})
 })
 
 cmp.setup.filetype('lisp', {
 	sources = cmp.config.sources({
-		{ name = 'vlime' },
-		{ name = 'buffer' },
+		{ name = 'vlime',  priority = 1 },
+		{ name = 'buffer', priority = 2 },
 	})
 })
 
 cmp.setup.filetype('gitcommit', {
 	sources = cmp.config.sources({
-		{ name = 'git', priority = 2 },
-		{ name = 'conventionalcommits', priority = 1 },
-		{ name = 'luasnip', priority = 3 },
-	}, {
-		{ name = 'buffer', priority = 4 },
+		{ name = 'git',                 priority = 3 },
+		{ name = 'conventionalcommits', priority = 2 },
+		{ name = 'luasnip',             priority = 1 },
+		{ name = 'buffer',              priority = 4 },
 	}),
 })
 
@@ -155,32 +162,32 @@ cmp.setup.cmdline({ '/', '?' }, {
 	mapping = cmp.mapping.preset.cmdline(),
 	sources = cmp.config.sources({
 		{ name = 'nvim_lsp_document_symbol', priority = 1 },
-	}, {
-		{ name = 'buffer', priority = 2 },
+		{ name = 'buffer',                   priority = 2 },
 	}),
 })
 
 cmp.setup.cmdline(':', {
 	mapping = cmp.mapping.preset.cmdline(),
 	sources = cmp.config.sources({
-		{ name = 'async_path', priority = 2 },
-	}, {
 		{
 			name = 'cmdline',
 			priority = 1,
-			option = {
-				treat_trailing_slash = false,
-			},
+			option = { treat_trailing_slash = false },
 		},
+		{ name = 'async_path', priority = 2 },
 	})
 })
 
--- sks.vscode()
+if is_fun(Sks.vscode) then
+	Sks.vscode()
+end
 
 -- For debugging.
 if exists('notify') then
-	require('notify')('cmp loaded.', 'info', {
-		title = 'nvim-cmp',
+	local Notify = require('notify')
+
+	Notify('cmp loaded.', 'info', {
+		title = 'cmp',
 	})
 else
 	print('cmp loaded.')
