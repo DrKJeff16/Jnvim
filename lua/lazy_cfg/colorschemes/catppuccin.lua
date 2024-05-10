9b3552de23ab5e0ef2aa5ce5ffe622847308f50b
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-05-09 20:02:30 -0600

    fix(color): Fixes for many colorschemes.

--- lua/lazy_cfg/colorschemes/catppuccin.lua
+++ lua/lazy_cfg/colorschemes/catppuccin.lua
@@ -21,53 +21,62 @@ if exists('catppuccin') then
 
 		---@type CatppuccinOptions
 		local opts = {
-			flavour = "mocha", -- latte, frappe, macchiato, mocha
-    		-- flavour = "auto" -- will respect terminal's background
-    		background = { -- :h background
-        		light = "latte",
-        		dark = "mocha",
-    		},
-    		transparent_background = false, -- disables setting the background color.
-    		show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
-    		term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
-    		dim_inactive = {
-        		enabled = false, -- dims the background color of inactive window
-        		shade = "dark",
-        		percentage = 0.10, -- percentage of the shade to apply to the inactive window
-    		},
-    		no_italic = true, -- Force no italic
-    		no_bold = false, -- Force no bold
-    		no_underline = false, -- Force no underline
-    		styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        		comments = {}, -- Change the style of comments
-        		conditionals = { "bold" },
-        		loops = { 'bold' },
-        		functions = { 'bold' },
-        		keywords = { 'bold' },
-        		strings = {},
-        		variables = {},
-        		numbers = {},
-        		booleans = { 'bold' },
-        		properties = {},
-        		types = {},
-        		operators = {},
-        		-- miscs = {}, -- Uncomment to turn off hard-coded styles
-    		},
-    		color_overrides = {},
-    		custom_highlights = {},
-    		default_integrations = true,
-    		integrations = {
-        		cmp = true,
-        		gitsigns = true,
-        		nvimtree = true,
-        		treesitter = true,
-        		notify = true,
-        		mini = {
-            		enabled = true,
-            		indentscope_color = "",
-        		},
-        		-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-    		},
+			flavour = "macchiato", -- latte, frappe, macchiato, mocha
+			-- flavour = "auto" -- will respect terminal's background
+			background = { -- :h background
+				light = "latte",
+				dark = "macchiato",
+			},
+			transparent_background = false, -- disables setting the background color.
+			show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
+			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+			dim_inactive = {
+				enabled = true, -- dims the background color of inactive window
+				shade = "dark",
+				percentage = 0.10, -- percentage of the shade to apply to the inactive window
+			},
+			no_italic = false, -- Force no italic
+			no_bold = false, -- Force no bold
+			no_underline = false, -- Force no underline
+			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+				comments = { 'altfont' }, -- Change the style of comments
+				conditionals = { "bold" },
+				loops = { 'bold' },
+				functions = { 'bold' },
+				keywords = { 'bold' },
+				strings = {},
+				variables = { 'altfont' },
+				numbers = {},
+				booleans = { 'bold' },
+				properties = { 'bold' },
+				types = { 'bold' },
+				operators = { 'altfont' },
+				-- miscs = {}, -- Uncomment to turn off hard-coded styles
+			},
+			color_overrides = {},
+			custom_highlights = {},
+			default_integrations = true,
+			integrations = {
+				barbar = exists('barbar'),
+				diffview = exists('diffview'),
+				treesitter_context = exists('treesitter-context'),
+				rainbow_delimiters = exists('rainbow-delimiters'),
+				indent_blankline = exists('ibl'),
+				native_lsp = true,
+				which_key = exists('which-key'),
+				telescope = exists('telescope'),
+				dashboard = exists('dashboard'),
+				cmp = exists('cmp'),
+				gitsigns = exists('gitsigns'),
+				nvimtree = exists('nvim-tree'),
+				treesitter = exists('nvim-treesitter'),
+				notify = exists('notify'),
+				mini = {
+					enabled = true,
+					indentscope_color = "",
+				},
+				-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+			},
 		}
 
 		Cppc.setup(opts)
--- lua/lazy_cfg/colorschemes/tokyonight.lua
+++ lua/lazy_cfg/colorschemes/tokyonight.lua
@@ -6,7 +6,6 @@ local Check = User.check
 local csc_m = User.types.colorschemes
 
 local exists = Check.exists.module
-local vim_exists = Check.exists.vim_exists
 
 local M = {
 	mod_pfx = 'lazy_cfg.colorschemes.tokyonight',
@@ -19,7 +18,40 @@ if exists('tokyonight') then
 
 		---@type Config
 		local opts = {
-			terminal_colors = vim_exists('+termguicolors'),
+			on_colors = function(colors)
+				colors.error = '#df4f4f'
+			end,
+			on_highlights = function(hl, c)
+				local prompt = '#2d3149'
+				hl.TelescopeNormal = {
+					bg = c.bg_dark,
+					fg = c.fg_dark
+				}
+				hl.TelescopeBorder = {
+					bg = c.bg_dark,
+					fg = c.bg_dark,
+				}
+				hl.TelescopePromptNormal = {
+					bg = prompt,
+				}
+				hl.TelescopePromptBorder = {
+					bg = prompt,
+					fg = prompt,
+				}
+				hl.TelescopePromptTitle = {
+					bg = prompt,
+					fg = prompt,
+				}
+				hl.TelescopePreviewTitle = {
+					bg = c.bg_dark,
+					fg = c.bg_dark,
+				}
+				hl.TelescopeResultsTitle = {
+					bg = c.bg_dark,
+					fg = c.bg_dark,
+				}
+			end,
+			terminal_colors = true,
 			transparent = false,
 			sidebars = {
 				'qf',
@@ -29,6 +61,7 @@ if exists('tokyonight') then
 				'terminal',
 				'packer',
 				'TelescopePrompt',
+				'vista_kind',
 			},
 
 			style = 'night',
@@ -36,6 +69,14 @@ if exists('tokyonight') then
 			use_background = true,
 			hide_inactive_statusline = false,
 			lualine_bold = true,
+			styles = {
+				comments = { italic = false },
+				keywords = { italic = false, bold = true },
+				functions = { bold = true, italic = false },
+				variables = {},
+				sidebars = 'dark',
+				floats = 'transparent',
+			},
 		}
 
 		TN.setup(opts)
