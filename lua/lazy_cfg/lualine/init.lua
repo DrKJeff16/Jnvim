243f2b91b77fb5176c3145821d5a3e99bbc05e85
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-05-09 20:04:20 -0600

    fix(lualine): Better theme selection, tweaks.

--- lua/lazy_cfg/lualine/init.lua
+++ lua/lazy_cfg/lualine/init.lua
@@ -5,6 +5,7 @@ local User = require('user')
 local Check = User.check
 
 local exists = Check.exists.module
+local is_str = Check.value.is_str
 
 if not exists('lualine') then
 	return
@@ -12,10 +13,30 @@ end
 
 local Lualine = require('lualine')
 
+---@type fun(theme: string?): string
+local function theme_select(theme)
+	if not is_str(theme) then
+		theme = 'auto'
+	else
+		theme = exists(theme) and theme or 'auto'
+	end
+
+	if theme == 'auto' then
+		for _, t in next, { 'catppuccin', 'tokyonight' } do
+			if exists(t) then
+				theme = t
+				break
+			end
+		end
+	end
+
+	return theme
+end
+
 Lualine.setup({
 	options = {
 		icons_enabled = true,
-        theme = 'auto',
+        theme = theme_select(),
         component_separators = { left = '|', right = '|'},
         section_separators = { left = '', right = ''},
         ignore_focus = {},
@@ -31,8 +52,7 @@ Lualine.setup({
         lualine_a = {
 			{
 				'mode',
-				---@param str string
-				---@return string
+				---@type fun(str: string): string
 				fmt = function(str)
 					return str:sub(1,1)
 				end,
