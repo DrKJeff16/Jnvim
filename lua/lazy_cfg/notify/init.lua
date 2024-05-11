54eaf008037eeff3b91a45f402a5ced6ca19a77b
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-05-10 23:29:23 -0600

    fix(notify): Options tweaked, refactored code, improved checks.

--- lua/lazy_cfg/notify/init.lua
+++ lua/lazy_cfg/notify/init.lua
@@ -10,7 +10,8 @@ local is_nil = Check.value.is_nil
 local is_str = Check.value.is_str
 local is_tbl = Check.value.is_tbl
 local exists = Check.exists.module
-local hl = Highlight.hl_from_arr
+local empty = Check.value.empty
+local hl = Highlight.hl
 
 if not exists('notify') then
 	return
@@ -21,7 +22,7 @@ local Notify = require('notify')
 ---@type notify.Config
 local Opts = {
 	background_colour = 'NotifyBackground',
-	fps = 48,
+	fps = 60,
 	icons = {
 		DEBUG = "",
 		ERROR = "",
@@ -29,64 +30,66 @@ local Opts = {
 		TRACE = "✎",
 		WARN = ""
 	},
-	level = 2,
-	minimum_width = 20,
+	level = vim.log.levels.INFO,
+	minimum_width = 44,
 	render = "compact",
 	stages = "slide",
 	time_formats = {
 		notification = "%T",
 		notification_history = "%FT%T"
 	},
-	timeout = 1500,
+	timeout = 1750,
 	top_down = false,
+	max_width = vim.o.columns * 0.6,
+	max_height = vim.o.lines * 0.4,
 }
 
 Notify.setup(Opts)
 vim.notify = Notify
 
----@param msg string
----@param lvl? 'info'|'error'|'warn'
----@param opts? table
+---@type fun(msg: string, lvl: ('info'|'error'|'warn')?, opts: table?)
 function _G.anotify(msg, lvl, opts)
-	local async = require('plenary.async').run
-
 	if not is_str(lvl) or not vim.tbl_contains({ 'info', 'warn', 'ertor' }, lvl) then
-		lvl = 'warn'
+		lvl = 'info'
 	end
 
-	if not is_tbl(opts) or not is_str(opts.title) then
+	if not is_tbl(opts) or not is_str(opts.title) or empty(opts.title) then
 		opts = {
-			title = 'Attention!',
+			title = 'Message',
 		}
 	end
+
+	local async = require('plenary.async').run
 
 	async(function()
 		Notify(msg, lvl, opts)
 	end)
 end
 
----@type HlPair[]
+---@type HlDict
 local NotifyHl = {
-	{ name = 'NotifyERRORBorder', opts = { fg = '#8A1F1F' } },
-	{ name = 'NotifyWARNBorder', opts = { fg = '#79491D' } },
-	{ name = 'NotifyINFOBorder', opts = { fg = '#4F6752' } },
-	{ name = 'NotifyDEBUGBorder', opts = { fg = '#8B8B8B' } },
-	{ name = 'NotifyTRACEBorder', opts = { fg = '#4F3552' } },
-	{ name = 'NotifyERRORIcon', opts = { fg = '#F70067' } },
-	{ name = 'NotifyWARNIcon', opts = { fg = '#F79000' } },
-	{ name = 'NotifyINFOIcon', opts = { fg = '#A9FF68' } },
-	{ name = 'NotifyDEBUGIcon', opts = { fg = '#8B8B8B' } },
-	{ name = 'NotifyTRACEIcon', opts = { fg = '#D484FF' } },
-	{ name = 'NotifyERRORTitle', opts = {  fg = '#F70067' } },
-	{ name = 'NotifyWARNTitle', opts = { fg = '#F79000' } },
-	{ name = 'NotifyINFOTitle', opts = { fg = '#A9FF68' } },
-	{ name = 'NotifyDEBUGTitle', opts = {  fg = '#8B8B8B' } },
-	{ name = 'NotifyTRACETitle', opts = {  fg = '#D484FF' } },
-	{ name = 'NotifyERRORBody', opts = { link = 'Normal' } },
-	{ name = 'NotifyWARNBody', opts = { link = 'Normal' } },
-	{ name = 'NotifyINFOBody', opts = { link = 'Normal' } },
-	{ name = 'NotifyDEBUGBody', opts = { link = 'Normal' } },
-	{ name = 'NotifyTRACEBody', opts = { link = 'Normal' } },
+	['NotifyERRORBorder'] = { fg = '#8A1F1F' },
+	['NotifyWARNBorder'] = { fg = '#79491D' },
+	['NotifyINFOBorder'] = { fg = '#4F6752' },
+	['NotifyDEBUGBorder'] = { fg = '#8B8B8B' },
+	['NotifyTRACEBorder'] = { fg = '#4F3552' },
+	['NotifyERRORIcon'] = { fg = '#F70067' },
+	['NotifyWARNIcon'] = { fg = '#F79000' },
+	['NotifyINFOIcon'] = { fg = '#A9FF68' },
+	['NotifyDEBUGIcon'] = { fg = '#8B8B8B' },
+	['NotifyTRACEIcon'] = { fg = '#D484FF' },
+	['NotifyERRORTitle'] = {  fg = '#F70067' },
+	['NotifyWARNTitle'] = { fg = '#F79000' },
+	['NotifyINFOTitle'] = { fg = '#A9FF68' },
+	['NotifyDEBUGTitle'] = {  fg = '#8B8B8B' },
+	['NotifyTRACETitle'] = {  fg = '#D484FF' },
+	['NotifyERRORBody'] = { link = 'Normal' },
+	['NotifyWARNBody'] = { link = 'Normal' },
+	['NotifyINFOBody'] = { link = 'Normal' },
+	['NotifyDEBUGBody'] = { link = 'Normal' },
+	['NotifyTRACEBody'] = { link = 'Normal' },
 }
 
-hl(NotifyHl)
+for name, opts in next, NotifyHl do
+	hl(name, opts)
+end
