7ca09afc733cd8b9e368d6ceaf2854e22b61ed5c
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-05-12 13:08:47 -0600

    fix(lualine): Formatting, sections added and modified.

--- lua/lazy_cfg/lualine/init.lua
+++ lua/lazy_cfg/lualine/init.lua
@@ -36,53 +36,74 @@ end
 Lualine.setup({
 	options = {
 		icons_enabled = true,
-        theme = theme_select(),
-        component_separators = { left = '|', right = '|'},
-        section_separators = { left = '', right = ''},
-        ignore_focus = {},
-        always_divide_middle = true,
-        globalstatus = false,
-        refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-        },
-    },
-    sections = {
-        lualine_a = {
+		theme = theme_select(),
+		component_separators = { left = '', right = '' },
+		section_separators = { left = '', right = '' },
+		ignore_focus = {},
+		always_divide_middle = true,
+		globalstatus = false,
+		refresh = {
+			statusline = 1000,
+			tabline = 1000,
+			winbar = 1000,
+		},
+	},
+	sections = {
+		lualine_a = {
 			{
 				'mode',
+				icons_enabled = true,
 				---@type fun(str: string): string
 				fmt = function(str)
-					return str:sub(1,1)
+					return str:sub(1, 1)
 				end,
 			},
-        },
-        lualine_b = { 'filename' },
-        lualine_c = { 'diagnostics' },
-        lualine_x = {
+		},
+		lualine_b = {
+			'branch',
+			'filename',
+		},
+		lualine_c = {
+			{
+				'diagnostics',
+				sources = { 'nvim_lsp', 'nvim_workspace_diagnostic' },
+				sections = { 'error', 'warn' },
+				diagnostics_color = {
+					error = 'DiagnosticError',
+					warn = 'DiagnosticWarn',
+					info = 'DiagnosticInfo',
+					hint = 'DiagnosticHint',
+				},
+				symbols = { error = 'E ', warn = 'W ', info = 'I ', hint = '? ' },
+				colored = true,
+				update_in_insert = false,
+				always_visible = true,
+			},
+		},
+		lualine_x = {
+			'encoding',
 			'fileformat',
 			'filetype',
-        },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
-    },
-    inactive_sections = {
-    	lualine_a = {},
-        lualine_b = { 'filename' },
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {'windows'},
-        lualine_z = {}
-    },
-    inactive_tabline = {},
-    inactive_winbar = {},
+		},
+		lualine_y = { 'progress' },
+		lualine_z = { 'location' }
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = { 'filename' },
+		lualine_c = {},
+		lualine_x = { 'location' },
+		lualine_y = { 'windows' },
+		lualine_z = {}
+	},
+	inactive_tabline = {},
+	inactive_winbar = {},
 
-    extensions = {
+	extensions = {
 		'lazy',
 		'fugitive',
 		'man',
 		'nvim-tree',
 		'toggleterm',
-    },
+	},
 })
