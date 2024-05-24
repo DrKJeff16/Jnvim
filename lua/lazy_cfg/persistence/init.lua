ff43b59a832a0c926627da3ab6b1e32c81d00583
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-05-23 20:14:10 -0600

    fix(persistence): Expanded options.

--- lua/lazy_cfg/persistence/init.lua
+++ lua/lazy_cfg/persistence/init.lua
@@ -7,6 +7,7 @@ local maps_t = User.types.user.maps
 local kmap = User.maps.kmap
 
 local exists = Check.exists.module
+local is_tbl = Check.value.is_tbl
 local nmap = kmap.n
 local desc = kmap.desc
 
@@ -14,10 +15,19 @@ if not exists('persistence') then
 	return
 end
 
+local expand = vim.fn.expand
+local stdpath = vim.fn.stdpath
+
 local Pst = require('persistence')
 
 local Opts = {
 	options = vim.opt.sessionoptions:get(),
+	dir = expand(stdpath("state") .. "/sessions/"), -- directory where session files are saved
+	pre_save = nil,                              -- a function to call before saving the session
+	post_save = nil,                             -- a function to call after saving the session
+	save_empty = false,                          -- don't save if there are no open file buffers
+	pre_load = nil,                              -- a function to call before loading the session
+	post_load = nil,                             -- a function to call after loading the session
 }
 
 Pst.setup(Opts)
@@ -30,9 +40,7 @@ local Keys = {
 		desc('Restore Session'),
 	},
 	['<leader>Sl'] = {
-		function()
-			Pst.load({ last = true })
-		end,
+		function() Pst.load({ last = true }) end,
 		desc('Restore Last Session'),
 	},
 	['<leader>Sd'] = {
@@ -40,3 +48,8 @@ local Keys = {
 		desc('Don\'t Save Current Session'),
 	},
 }
+
+for lhs, v in next, Keys do
+	v[2] = is_tbl(v[2]) and v[2] or {}
+	nmap(lhs, v[1], v[2])
+end
