db2d87da73c305918fbdf7b2f6f2c971a9d30fde
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-05-25 20:22:58 -0600

    chore(color): `catppuccin` corrections and file formatting.

--- lua/lazy_cfg/colorschemes/catppuccin.lua
+++ lua/lazy_cfg/colorschemes/catppuccin.lua
@@ -27,16 +27,16 @@ if exists("catppuccin") then
 			},
 			transparent_background = false, -- disables setting the background color.
 			show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
-			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+			term_colors = false,   -- sets terminal colors (e.g. `g:terminal_color_0`)
 			dim_inactive = {
-				enabled = true, -- dims the background color of inactive window
+				enabled = true,    -- dims the background color of inactive window
 				shade = "dark",
 				percentage = 0.15, -- percentage of the shade to apply to the inactive window
 			},
-			no_italic = true, -- Force no italic
-			no_bold = false, -- Force no bold
-			no_underline = false, -- Force no underline
-			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+			no_italic = false,     -- Force no italic
+			no_bold = false,       -- Force no bold
+			no_underline = false,  -- Force no underline
+			styles = {             -- Handles the styles of general hi groups (see `:h highlight-args`):
 				comments = { "altfont" }, -- Change the style of comments
 				conditionals = { "altfont" },
 				loops = { "bold" },
