a31143e07f1994d04ada9f2f898cd07a82573b4b
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-05-26 18:31:03 -0600

    fix(color): Tweaked `catppuccin` integrations.

--- lua/lazy_cfg/colorschemes/catppuccin.lua
+++ lua/lazy_cfg/colorschemes/catppuccin.lua
@@ -19,14 +19,14 @@ if exists('catppuccin') then
 
 		---@type CatppuccinOptions
 		local opts = {
-			flavour = 'mocha', -- latte, frappe, macchiato, mocha
+			flavour = 'frappe', -- latte, frappe, macchiato, mocha
 			-- flavour = "auto" -- will respect terminal's background
 			background = { -- :h background
 				light = 'latte',
 				dark = 'mocha',
 			},
 			transparent_background = false, -- disables setting the background color.
-			show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
+			show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
 			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
 			dim_inactive = {
 				enabled = true, -- dims the background color of inactive window
@@ -38,40 +38,71 @@ if exists('catppuccin') then
 			no_underline = false, -- Force no underline
 			styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
 				comments = { 'altfont' }, -- Change the style of comments
-				conditionals = { 'altfont' },
+				conditionals = { 'bold' },
 				loops = { 'bold' },
 				functions = { 'bold' },
 				keywords = { 'bold' },
-				strings = { 'altfont' },
+				strings = { 'italic' },
 				variables = { 'altfont' },
 				numbers = { 'altfont' },
 				booleans = { 'altfont' },
-				properties = { 'underline' },
+				properties = { 'altfont' },
 				types = { 'undercurl' },
 				operators = { 'altfont' },
 				-- miscs = {}, -- Uncomment to turn off hard-coded styles
 			},
-			color_overrides = {},
-			custom_highlights = {},
+
+			--[[ color_overrides = {},
+			custom_highlights = {}, ]]
+
 			default_integrations = true,
 			integrations = {
 				barbar = exists('barbar'),
-				diffview = exists('diffview'),
-				treesitter_context = exists('treesitter-context'),
-				rainbow_delimiters = exists('rainbow-delimiters'),
-				indent_blankline = exists('ibl'),
-				native_lsp = true,
-				which_key = exists('which-key'),
-				telescope = exists('telescope'),
-				dashboard = exists('dashboard'),
 				cmp = exists('cmp'),
+				dashboard = exists('dashboard'),
+				diffview = exists('diffview'),
 				gitsigns = exists('gitsigns'),
-				nvimtree = exists('nvim-tree'),
-				treesitter = exists('nvim-treesitter'),
+				indent_blankline = {
+					enabled = exists('ibl'),
+					scope_color = 'teal',
+					colored_indent_levels = true,
+				},
+				lsp_trouble = exists('trouble'),
+				markdown = true,
+				native_lsp = {
+					enabled = true,
+					virtual_text = {
+						errors = { 'bold', 'undercurl' },
+						warnings = { 'underline' },
+						hints = { 'italic' },
+						information = { 'altfont', 'underline' },
+						ok = { 'bold' },
+					},
+					underlines = {
+						errors = { 'underline' },
+						hints = { 'underline' },
+						warnings = { 'underline' },
+						information = { 'underline' },
+						ok = { 'underline' },
+					},
+					inlay_hints = {
+						background = true,
+					},
+				},
+				noice = exists('noice'),
 				notify = exists('notify'),
+				nvimtree = exists('nvim-tree'),
+				rainbow_delimiters = exists('rainbow-delimiters'),
+				telescope = {
+					enabled = exists('telescope'),
+					-- style = 'nvchad',
+				},
+				treesitter = exists('nvim-treesitter'),
+				treesitter_context = exists('treesitter-context'),
+				which_key = exists('which-key'),
 				mini = {
 					enabled = true,
-					indentscope_color = '',
+					indentscope_color = 'lavender',
 				},
 				-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
 			},
