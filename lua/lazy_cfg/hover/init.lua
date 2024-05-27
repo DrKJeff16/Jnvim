1beda2e76e7c79fea7e9c13a6fe80d65422edaa7
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-05-26 22:17:57 -0600

    misc(hover): Code improvements, API integration.

--- lua/lazy_cfg/hover/init.lua
+++ lua/lazy_cfg/hover/init.lua
@@ -7,7 +7,9 @@ local maps_t = User.types.user.maps
 local kmap = User.maps.kmap
 
 local exists = Check.exists.module
+local is_tbl = Check.value.is_tbl
 local nmap = kmap.n
+local desc = kmap.desc
 
 if not exists('hover') then
 	return
@@ -15,17 +17,21 @@ end
 
 local Hover = require('hover')
 
-Hover.setup({
+---@type Hover.Config
+local Opts = {
 	init = function()
 		-- Providers
-		require('hover.providers.lsp')
+		if exists('lspconfig') then
+			require('hover.providers.lsp')
+		end
+
 		require('hover.providers.man')
 
 		--- Github
-		-- require('hover.providers.gh')
+		require('hover.providers.gh')
 
 		--- Github: Users
-		-- require('hover.providers.gh_user')
+		require('hover.providers.gh_user')
 
 		--- Jira
 		-- require('hover.providers.jira')
@@ -41,26 +47,35 @@ Hover.setup({
 	title = true,
 	mouse_providers = { 'LSP' },
 	mouse_delay = 1000,
-})
+}
+
+Hover.setup(Opts)
+
+---@type Hover.Options
+local HOpts = {
+	bufnr = vim.api.nvim_win_get_buf(vim.api.nvim_get_current_win()),
+	pos = vim.api.nvim_win_get_cursor(vim.api.nvim_get_current_win()),
+}
 
 ---@type KeyMapDict
 local Keys = {
-	['K'] = { Hover.hover, { desc = 'Hover' } },
-	['gK'] = { Hover.hover_select, { desc = 'Hover Select' } },
+	['K'] = { Hover.hover, desc('Hover') },
+	['gK'] = { Hover.hover_select, desc('Hover Select') },
 	['<C-p>'] = {
 		function()
-			Hover.hover_switch('previous')
+			Hover.hover_switch('previous', HOpts)
 		end,
-		{ desc = 'Previous Hover' },
+		desc('Previous Hover'),
 	},
 	['<C-n>'] = {
 		function()
-			Hover.hover_switch('next')
+			Hover.hover_switch('next', HOpts)
 		end,
-		{ desc = 'Next Hover' },
+		desc('Next Hover'),
 	},
 }
 
 for lhs, v in next, Keys do
+	v[2] = is_tbl(v[2]) and v[2] or {}
 	nmap(lhs, v[1], v[2] or {})
 end
