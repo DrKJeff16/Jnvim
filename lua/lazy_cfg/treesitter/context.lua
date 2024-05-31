dd2767094c630fc721cceca86ead9d07b5000d9f
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-05-30 19:23:48 -0600

    fix(ts): `wk` integration for `context`.

--- lua/lazy_cfg/treesitter/context.lua
+++ lua/lazy_cfg/treesitter/context.lua
@@ -6,12 +6,12 @@ local Check = User.check
 local hl_t = User.types.user.highlight
 local map_t = User.types.user.maps
 local kmap = User.maps.kmap
+local WK = User.maps.wk
 
 local exists = Check.exists.module
 local is_tbl = Check.value.is_tbl
 local empty = Check.value.empty
 local hi = User.highlight.hl
-local nmap = kmap.n
 local desc = kmap.desc
 
 if not exists('treesitter-context') then
@@ -42,19 +42,36 @@ local hls = {
 	},
 }
 
----@type KeyMapDict
-local keys = {
-	['<leader>Cn'] = {
-		function()
-			Context.goto_context(vim.v.count1)
-		end,
-		desc('Previous Context'),
+---@type table<MapModes, KeyMapDict>
+local Keys = {
+	n = {
+		['<leader>Cn'] = {
+			function()
+				Context.goto_context(vim.v.count1)
+			end,
+			desc('Previous Context'),
+		},
 	},
 }
+---@type table<MapModes, RegKeysNamed>
+local Names = {
+	n = { ['<leader>C'] = { name = '+Context' } },
+}
 
-for lhs, v in next, keys do
-	v[2] = is_tbl(v[2]) and v[2] or {}
-	nmap(lhs, v[1], v[2])
+for mode, t in next, Keys do
+	if WK.available() then
+		if is_tbl(Names[mode]) and not empty(Names[mode]) then
+			WK.register(Names[mode], { mode = mode })
+		end
+
+		WK.register(WK.convert_dict(t), { mode = mode })
+	else
+		for lhs, v in next, t do
+			v[2] = is_tbl(v[2]) and v[2] or {}
+
+			kmap[mode](lhs, v[1], v[2])
+		end
+	end
 end
 
 for k, v in next, hls do
