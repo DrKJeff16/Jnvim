2431c463c8477c7ed3aeac970de2e8b9fdb86f09
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-06-03 18:50:27 -0600

    chore(api): Add and reorder dirs.

--- lua/user/distro/archlinux.lua
+++ lua/user/distro/archlinux.lua
@@ -1,7 +1,7 @@
 -- Modify runtimepath to also search the system-wide Vim directory
 -- (eg. for Vim runtime files from Arch Linux packages)
 
-local types = require('user.types')
+require('user.types')
 local Check = require('user.check')
 local Util = require('user.util')
 
@@ -15,9 +15,10 @@ local M = {}
 
 function M.setup()
 	local rtpaths = {
+		'/usr/local/share/nvim/runtime',
+		'/usr/share/nvim/runtime',
 		'/usr/local/share/vim/vimfiles',
 		'/usr/local/share/vim/vimfiles/after',
-		'/usr/share/nvim/runtime',
 		'/usr/share/vim/vimfiles',
 		'/usr/share/vim/vimfiles/after',
 	}
