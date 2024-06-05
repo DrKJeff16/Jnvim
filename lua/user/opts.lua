fae1a4a3c316c9d961eacf129c12703244c237f2
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-06-05 16:08:17 -0600

    fix: We're gonna use spaces, I'm capitulating.

--- lua/user/opts.lua
+++ lua/user/opts.lua
@@ -16,90 +16,90 @@ local notify = Util.notify.notify
 
 ---@type OptsTbl
 local opt_tbl = {
-	autoindent = true,
-	autoread = true,
-	backspace = { 'indent', 'eol', 'start' },
-	backup = false,
-	belloff = { 'all' },
-	background = 'dark',
-	copyindent = true,
-	cmdwinheight = 3,
-	colorcolumn = { '+1' },
-	completeopt = { 'menu', 'menuone', 'noselect', 'noinsert', 'preview' },
-	confirm = true,
-	encoding = 'utf-8',
-	equalalways = true,
-	errorbells = false,
-	expandtab = false,
-	fileencoding = 'utf-8',
-	fileignorecase = false,
-	formatoptions = 'bjlopqnw',
-	hidden = true,
-	helplang = { 'en' },
-	hlsearch = true,
-	ignorecase = false,
-	incsearch = true,
-	laststatus = 2,
-	makeprg = 'make',
-	matchpairs = {
-		'(:)',
-		'[:]',
-		'{:}',
-		'<:>',
-	},
-	matchtime = 30,
-	menuitems = 40,
-	mouse = '', -- Get that mouse out of my sight!
-	number = true,
-	preserveindent = true,
-	relativenumber = false,
-	ruler = true,
-	sessionoptions = {
-		'buffers',
-		'tabpages',
-		'globals',
-	},
-	shell = executable('bash') and 'bash' or 'sh',
-	scrolloff = 3,
-	showcmd = true,
-	showmatch = true,
-	showmode = false,
-	smartindent = true,
-	signcolumn = 'yes',
-	smartcase = true,
-	spell = false,
-	splitbelow = true,
-	splitright = true,
-	smarttab = true,
-	showtabline = 2,
-	softtabstop = 4,
-	shiftwidth = 0,
-	termguicolors = vim_exists('+termguicolors') and not in_console(),
-	title = true,
-	tabstop = 4,
-	updatecount = 100,
-	updatetime = 1000,
-	visualbell = false,
-	wildmenu = true,
-	wrap = false,
+    autoindent = true,
+    autoread = true,
+    backspace = { 'indent', 'eol', 'start' },
+    backup = false,
+    belloff = { 'all' },
+    background = 'dark',
+    copyindent = true,
+    cmdwinheight = 3,
+    colorcolumn = { '+1' },
+    completeopt = { 'menu', 'menuone', 'noselect', 'noinsert', 'preview' },
+    confirm = true,
+    encoding = 'utf-8',
+    equalalways = true,
+    errorbells = false,
+    expandtab = true,
+    fileencoding = 'utf-8',
+    fileignorecase = false,
+    formatoptions = 'bjlopqnw',
+    hidden = true,
+    helplang = { 'en' },
+    hlsearch = true,
+    ignorecase = false,
+    incsearch = true,
+    laststatus = 2,
+    makeprg = 'make',
+    matchpairs = {
+        '(:)',
+        '[:]',
+        '{:}',
+        '<:>',
+    },
+    matchtime = 30,
+    menuitems = 40,
+    mouse = '', -- Get that mouse out of my sight!
+    number = true,
+    preserveindent = true,
+    relativenumber = false,
+    ruler = true,
+    sessionoptions = {
+        'buffers',
+        'tabpages',
+        'globals',
+    },
+    shell = executable('bash') and 'bash' or 'sh',
+    scrolloff = 3,
+    showcmd = true,
+    showmatch = true,
+    showmode = false,
+    smartindent = true,
+    signcolumn = 'yes',
+    smartcase = true,
+    spell = false,
+    splitbelow = true,
+    splitright = true,
+    smarttab = true,
+    showtabline = 2,
+    softtabstop = 4,
+    shiftwidth = 4,
+    termguicolors = vim_exists('+termguicolors') and not in_console(),
+    title = true,
+    tabstop = 4,
+    updatecount = 100,
+    updatetime = 1000,
+    visualbell = false,
+    wildmenu = true,
+    wrap = false,
 }
 
 if is_windows then
-	opt_tbl.fileignorecase = true
-	opt_tbl.makeprg = executable('mingw32-make.exe') and 'mingw32-make.exe' or opt_tbl.makeprg
+    opt_tbl.fileignorecase = true
+    opt_tbl.makeprg = executable('mingw32-make.exe') and 'mingw32-make.exe' or opt_tbl.makeprg
 
-	opt_tbl.shell = 'cmd.exe'
-	if executable('bash.exe') then
-		opt_tbl.shell = 'bash.exe'
-		opt_tbl.shellcmdflag = '-c'
-	elseif executable('sh.exe') then
-		opt_tbl.shell = 'sh.exe'
-		opt_tbl.shellcmdflag = '-c'
-	else
-		opt_tbl.shell = 'cmd.exe'
-	end
+    opt_tbl.shell = 'cmd.exe'
+    if executable('bash.exe') then
+        opt_tbl.shell = 'bash.exe'
+        opt_tbl.shellcmdflag = '-c'
+    elseif executable('sh.exe') then
+        opt_tbl.shell = 'sh.exe'
+        opt_tbl.shellcmdflag = '-c'
+    else
+        opt_tbl.shell = 'cmd.exe'
+    end
 
-	opt_tbl.shellslash = true
+    opt_tbl.shellslash = true
 end
 
 --- Option setter for the aforementioned options dictionary.
@@ -110,15 +110,15 @@ end
 --- ---
 ---@type fun(opts: OptsTbl)
 local function optset(opts)
-	for k, v in next, opts do
-		if not is_nil(vim.opt[k]) then
-			vim.opt[k] = v
-		elseif not is_nil(vim.o[k]) then
-			vim.o[k] = v
-		else
-			notify('(user.opts:optset): Unable to set option `' .. k .. '`', 'error', { title = 'user.opts' })
-		end
-	end
+    for k, v in next, opts do
+        if not is_nil(vim.opt[k]) then
+            vim.opt[k] = v
+        elseif not is_nil(vim.o[k]) then
+            vim.o[k] = v
+        else
+            notify('(user.opts:optset): Unable to set option `' .. k .. '`', 'error', { title = 'user.opts' })
+        end
+    end
 end
 
 optset(opt_tbl)
