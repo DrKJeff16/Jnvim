edc72fc022d6898b1dff9cbc08efadb860338e72
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-06-06 17:23:05 -0600

    fix(api): Prepare `lualine` types for changes.

--- lua/user/types/lualine.lua
+++ lua/user/types/lualine.lua
@@ -1,116 +1,144 @@
----@diagnostic disable:unused-local
----@diagnostic disable:unused-function
+---@alias SectionComponentStr
+---|'mode'
+---|'filetype'
+---|'encoding'
+---|'fileformat'
+---|'filename'
+---|'branch'
+---|'diff'
+---|'diagnostics'
+---|'datetime'
+---|'progress'
+---|'location'
+---|'buffers'
+---|'filesize'
+---|'hostname'
+---|'searchcount'
+---|'selectioncount'
+---|'tabs'
+---|'windows'
+---|string
 
--- Displays diagnostics for the defined severity types
----@alias DiagLvl ('error'|'warn'|'info'|'hint')
----@alias DiagLvls DiagLvl[]
+---@class SectionSeparator
+---@field left string
+---@field right string
 
----@alias TabLine.Comps.Diag.Spec table<DiagLvl, any>
----@alias TabLine.Comps.Diag.Src table<DiagLvl, any>
----@alias TabLine.Comps.Diag.Colors table<DiagLvl, any>
----@alias TabLine.Comps.Diag.Symbols table<DiagLvl, any>
-
----@alias Tabline.Comps.DateTime.style 'default'|'us'|'uk'|'iso'|string
-
---- Table of diagnostic sources, available sources are:
----   `'nvim_lsp'`, `'nvim_diagnostic'`, `'nvim_workspace_diagnostic'`, `'coc'`, `'ale'`, `'vim_lsp'`.
---- or a function that returns a table as such:
---- ```lua
---- {
----		error = error_count,
----		warn = warn_count,
----		info = info_count,
----		hint = hint_count
---- }
---- ```
----@alias DiagOpt ('nvim_lsp'|'nvim_diagnostic'|'nvim_workspace_diagnostic'|'coc'|'ale'|'vim_lsp')[]|fun(): TabLine.Comps.Diag.Src
-
----@class TabLine.Comps.Spec
+---@class LuaLine.Components.Spec
+---@field [1] SectionComponentStr
 ---@field icons_enabled? boolean
----@field icon? any
----@field separator? table<string, string>|nil
----@field cond? any
+---@field icon? string|nil
+---@field separator? nil|string|SectionSeparator
+---@field cond? nil|fun()
 ---@field draw_empty? boolean
----@field color? table<string, string>|nil
----@field type? 'lua_expr'|'vim_fun'|nil
----@field on_click? nil|fun(...)
+---@field color? nil|vim.api.keyset.highlight|string
+---@field type? any
+---@field padding? integer
+---@field fmt? nil|fun(str: string, context: any?)
+---@field on_click? nil|fun(clicks: integer?, button: string, mods: any)
 
----@class TabLine.Comps.Buffer: TabLine.Comps.Spec
+---@class LuaLine.Components.Buffers: LuaLine.Components.Spec
+---@field [1] 'buffers'
 ---@field show_filename_only? boolean
 ---@field hide_filename_extension? boolean
 ---@field show_modified_status? boolean
+---@field mode? 0|1|2|3|4
+---@field max_length? number
+---@field filetype_names? table<string, string>
+---@field use_mode_colors? boolean
+---@field buffers_color? table<'active'|'inactive', string>
+---@field symbols? table<'modified'|'alternate_file'|'directory', string>
 
----@class TabLine.Comps.Buffer.Colors
----@field active? string
----@field inactive? string
+---@class LuaLine.Components.DateTime: LuaLine.Components.Spec
+---@field [1] 'datetime'
+---@field style? 'default'|'us'|'uk'|'iso'|string
 
----@class TabLine.Comps.Buffer.Symbols
----@field modified? string
----@field alternate_file? string
----@field directory? string
+---@class LuaLine.Components.Diagnostics: LuaLine.Components.Spec
+---@field [1] 'diagnostics'
+---@field sources?
+---|('nvim_lsp'|'nvim_diagnostic'|'nvim_workspace_diagnostic'|'coc'|'ale'|'vim_lsp')[]
+---|fun(...): table<'error'|'warn'|'info'|'hint', integer>
+---@field sections? ('error'|'warn'|'info'|'hint')[]
+---@field diagnostics_color? table<'error'|'warn'|'info'|'hint', string>
+---@field symbols? table<'error'|'warn'|'info'|'hint', string>
+---@field colored? boolean
+---@field update_in_insert? boolean
+---@field always_visible? boolean
 
----@class TabLine.Comps.Diff.Colors
----@field added? string
----@field modified? string
----@field removed? string
+---@class LuaLine.Components.Diff: LuaLine.Components.Spec
+---@field [1] 'diff'
+---@field colored? boolean
+---@field diff_color? table<'added'|'modified'|'removed', string>
+---@field symbols? table<'added'|'modified'|'removed', string>
+---@field source?
+---|fun(...): (nil|table<'added'|'modified'|'removed', integer>)
+---|integer
 
----@class TabLine.Comps.Diff.Symbols: TabLine.Comps.Diff.Colors
----@class TabLine.Comps.Diff.Src: TabLine.Comps.Diff.Colors
+---@class LuaLine.Components.Fileformat: LuaLine.Components.Spec
+---@field [1] 'fileformat'
+---@field symbols? table<'unix'|'dos'|'mac', string>
 
----@class TabLine.Comps.FF.Symbols
----@field unix? string
----@field dos? string
----@field mac? string
+---@class LuaLine.Components.Filename: LuaLine.Components.Spec
+---@field [1] 'filename'
+---@field file_status? boolean
+---@field newfile_status? boolean
+---@field path? 0|1|2|3|4
+---@field shorting_target? integer
+---@field symbols? table<'modified'|'readonly'|'unnamed'|'newfile', string>
 
----@class TabLine.Comps.DateTime: TabLine.Comps.Spec
----@class TabLine.Comps.Diag: TabLine.Comps.Spec
----@class TabLine.Comps.Diff: TabLine.Comps.Spec
----@class TabLine.Comps.File: TabLine.Comps.Spec
----@class TabLine.Comps.FT: TabLine.Comps.Spec
+---@class LuaLine.Components.Filetype: LuaLine.Components.Spec
+---@field [1] 'filetype'
+---@field colored? boolean
+---@field icon_only? boolean
+---@field icon?
+---|{ ['align']: string }
+---|{ [1]: string, ['align']: string }
 
----@class TabLine.Comps.FF: TabLine.Comps.Spec
----@field symbols? TabLine.Comps.FF.Symbols
-
----@class TabLine.Comps.SC: TabLine.Comps.Spec
+---@class LuaLine.Components.Searchcount: LuaLine.Components.Spec
+---@field [1] 'searchcount'
 ---@field maxcount? integer
----@field timeout? number
+---@field timeout? integer
 
----@alias TabLine.AllComps
----|TabLine.Comps.Buffer
----|TabLine.Comps.DateTime
----|TabLine.Comps.Diag
----|TabLine.Comps.Diff
----|TabLine.Comps.FF
----|TabLine.Comps.FT
----|TabLine.Comps.File
----|TabLine.Comps.SC
----|TabLine.Comps.Spec
----|string
----|table
+---@class LuaLine.Components.Tabs: LuaLine.Components.Spec
+---@field [1] 'tabs'
+---@field tab_max_length? integer
+---@field max_length? number
+---@field mode? 0|1|2
+---@field path? 0|1|2|3
+---@field use_mode_colors? boolean
+---@field tabs_color? table<'active'|'inactive', string>
+---@field show_modified_status? boolean
+---@field symbols? { ['modified']: string }
+---@field fmt? fun(name: string, context: table): string
 
----@alias Sections ('lualine_a'|'lualine_b'|'lualine_c'|'lualine_x'|'lualine_y'|'lualine_z')
+---@class LuaLine.Components.Windows: LuaLine.Components.Spec
+---@field show_filename_only? boolean
+---@field show_modified_status? boolean
+---@field mode? 0|1|2
+---@field max_length? number
+---@field filetype_names? table<string, string>
+---@field diabled_buftypes? string[]
+---@field use_mode_colors? boolean
+---@field windows_color? table<'active'|'inactive', string>
 
----@alias TabLine.Sections.Spec table<Sections, table|(TabLine.AllComps)[]>
+---@alias LuaLine.Components
+---|LuaLine.Components.Buffers
+---|LuaLine.Components.Diagnostics
+---|LuaLine.Components.Diff
+---|LuaLine.Components.Fileformat
+---|LuaLine.Components.Filename
+---|LuaLine.Components.Filetype
+---|LuaLine.Components.Searchcount
+---|LuaLine.Components.Tabs
+---|LuaLine.Components.Windows
+---|LuaLine.Components.DateTime
 
----@class TabLine.Sections
----@field active TabLine.Sections.Spec
----@field inactive TabLine.Sections.Spec
+---@class LuaLine.Sections
+---@field lualine_a (table|LuaLine.Components|SectionComponentStr)[]
+---@field lualine_b (table|LuaLine.Components|SectionComponentStr)[]
+---@field lualine_c (table|LuaLine.Components|SectionComponentStr)[]
+---@field lualine_x (table|LuaLine.Components|SectionComponentStr)[]
+---@field lualine_y (table|LuaLine.Components|SectionComponentStr)[]
+---@field lualine_z (table|LuaLine.Components|SectionComponentStr)[]
 
----@class TabLine.Comps
----@field basic? TabLine.Comps.Spec
----@field buffers? TabLine.Comps.Buffer
----@field datetime? TabLine.Comps.DateTime
----@field diagnostics? TabLine.Comps.Diag
----@field diff? TabLine.Comps.Diff
----@field fileformat? TabLine.Comps.FF
----@field filetype? TabLine.Comps.FT
----@field filename? TabLine.Comps.File
----@field searchcount? TabLine.Comps.SC
-
----@class TabLine.Preset
----@field default table
-
----@class TabLine
----@field components TabLine.Comps
----@field new fun(): TabLine
----@field __index? TabLine
+---@class LuaLine.Presets
+---@field default LuaLine.Sections
