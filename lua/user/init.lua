79ab63ddfbb2475385a1ba4b46c21c98cf8d5d16
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-06-08 00:12:44 -0600

    fix(api): Remove deprecation.

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -22,9 +22,6 @@ local M = {
     opts = require('user.opts'),
     distro = require('user.distro'),
     update = require('user.update'),
-
-    ---@deprecated
-    assoc = Util.assoc,
 }
 
 return M
