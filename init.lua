aa4a415c4b01429823615b72a0e2d70b555d575d
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-06-13 20:29:39 -0600

    misc(init): Sorted keymaps, formatting.

--- init.lua
+++ init.lua
@@ -4,11 +4,11 @@
 local User = require('user') -- User API
 local Check = User.check -- Checking utilities
 local Types = User.types -- Import docstrings and annotations
-local maps_t = Types.user.maps -- Annotations for mapping utilities
-local Kmap = User.maps.kmap -- `vim.keymap.set` backend
-local WK = User.maps.wk -- `which-key` backend
+local Maps = User.maps -- Mapping utilities
 local Util = User.util -- General utilities
-local Notify = Util.notify -- API notifier
+local Kmap = Maps.kmap -- `vim.keymap.set` backend
+local WK = Maps.wk -- `which-key` backend
+local maps_t = Types.user.maps -- Annotations for mapping utilities
 
 local exists = Check.exists.module -- Checks for missing modules
 local is_nil = Check.value.is_nil
@@ -20,7 +20,7 @@ local vim_has = Check.exists.vim_has
 local nop = User.maps.nop
 local desc = Kmap.desc
 local ft_get = Util.ft_get
-local notify = Notify.notify
+local notify = Util.notify.notify
 
 _G.is_windows = vim_has('win32')
 
@@ -87,12 +87,14 @@ local Keys = {
     n = {
         ['<Esc><Esc>'] = { vim.cmd.nohls, desc('Remove Highlighted Search') },
 
-        ['<leader>fr'] = { ':%s/', desc('Run Search-Replace Prompt For Whole File', false) },
-        ['<leader>fir'] = { ':%retab<CR>', desc('Retab File') },
-        ['<leader>fFo'] = { ':%foldopen<CR>', desc('Open All Folds') },
         ['<leader>fFc'] = { ':%foldclose<CR>', desc('Close All Folds') },
-        ['<leader>fs'] = { ':w<CR>', desc('Save File') },
+        ['<leader>fFo'] = { ':%foldopen<CR>', desc('Open All Folds') },
         ['<leader>fS'] = { ':w ', desc('Save File (Prompt)', false) },
+        ['<leader>fir'] = { ':%retab<CR>', desc('Retab File') },
+        ['<leader>fr'] = { ':%s/', desc('Run Search-Replace Prompt For Whole File', false) },
+        ['<leader>fs'] = { ':w<CR>', desc('Save File') },
+        ['<leader>fvL'] = { ':luafile ', desc('Source Lua File (Prompt)', false) },
+        ['<leader>fvV'] = { ':so ', desc('Source VimScript File (Prompt)', false) },
         ['<leader>fvl'] = {
             function()
                 local ft = Util.ft_get()
@@ -129,12 +131,10 @@ local Keys = {
             end,
             desc('Source Current File As VimScript File'),
         },
-        ['<leader>fvV'] = { ':so ', desc('Source VimScript File (Prompt)', false) },
-        ['<leader>fvL'] = { ':luafile ', desc('Source Lua File (Prompt)', false) },
 
-        ['<leader>vet'] = { '<CMD>tabnew $MYVIMRC<CR>', desc('Open In New Tab') },
         ['<leader>vee'] = { '<CMD>ed $MYVIMRC<CR>', desc('Open In Current Window') },
         ['<leader>ves'] = { '<CMD>split $MYVIMRC<CR>', desc('Open In Horizontal Split') },
+        ['<leader>vet'] = { '<CMD>tabnew $MYVIMRC<CR>', desc('Open In New Tab') },
         ['<leader>vev'] = { '<CMD>vsplit $MYVIMRC<CR>', desc('Open In Vertical Split') },
         ['<leader>vh'] = { '<CMD>checkhealth<CR>', desc('Run Checkhealth') },
         ['<leader>vs'] = {
@@ -145,95 +145,96 @@ local Keys = {
             desc('Source $MYVIMRC'),
         },
 
-        ['<leader>ht'] = { ':tab h ', desc('Prompt For Help On New Tab', false) },
-        ['<leader>hv'] = { ':vertical h ', desc('Prompt For Help On Vertical Split', false) },
-        ['<leader>hs'] = { ':horizontal h ', desc('Prompt For Help On Horizontal Split', false) },
-        ['<leader>hh'] = { ':h ', desc('Prompt For Help', false) },
+        ['<leader>hS'] = { ':horizontal h<CR>', desc('Open Help On Horizontal Split') },
         ['<leader>hT'] = { ':tab h<CR>', desc('Open Help On New Tab') },
         ['<leader>hV'] = { ':vertical h<CR>', desc('Open Help On Vertical Split') },
-        ['<leader>hS'] = { ':horizontal h<CR>', desc('Open Help On Horizontal Split') },
+        ['<leader>hh'] = { ':h ', desc('Prompt For Help', false) },
+        ['<leader>hs'] = { ':horizontal h ', desc('Prompt For Help On Horizontal Split', false) },
+        ['<leader>ht'] = { ':tab h ', desc('Prompt For Help On New Tab', false) },
+        ['<leader>hv'] = { ':vertical h ', desc('Prompt For Help On Vertical Split', false) },
 
-        ['<leader>wn'] = { '<C-w>w', desc('Cycle Window') },
         ['<leader>wd'] = { '<C-w>q', desc('Close Window') },
+        ['<leader>wn'] = { '<C-w>w', desc('Cycle Window') },
         ['<leader>wsS'] = { ':split ', desc('Horizontal Split (Prompt)', false) },
         ['<leader>wsV'] = { ':vsplit ', desc('Vertical Split (Prompt)', false) },
         ['<leader>wss'] = { ':split<CR>', desc('Horizontal Split', false) },
         ['<leader>wsv'] = { ':vsplit<CR>', desc('Vertical Split', false) },
 
-        ['<leader>qq'] = { ':qa<CR>', desc('Quit Nvim', false) },
         ['<leader>qQ'] = { ':qa!<CR>', desc('Quit Nvim Forcefully', false) },
+        ['<leader>qq'] = { ':qa<CR>', desc('Quit Nvim', false) },
 
-        ['<leader>ta'] = { ':tabnew ', desc('New Tab (Prompt)', false) },
-        ['<leader>tn'] = { ':tabN<CR>', desc('Next Tab', false) },
-        ['<leader>tp'] = { ':tabp<CR>', desc('Previous Tab', false) },
-        ['<leader>td'] = { ':tabc<CR>', desc('Close Tab', false) },
+        ['<leader>tA'] = { ':tabnew<CR>', desc('New Tab', false) },
         ['<leader>tD'] = { ':tabc!<CR>', desc('Close Tab Forcefully', false) },
+        ['<leader>ta'] = { ':tabnew ', desc('New Tab (Prompt)', false) },
+        ['<leader>td'] = { ':tabc<CR>', desc('Close Tab', false) },
         ['<leader>tf'] = { ':tabfirst<CR>', desc('Goto First Tab', false) },
         ['<leader>tl'] = { ':tablast<CR>', desc('Goto Last Tab', false) },
-        ['<leader>tA'] = { ':tabnew<CR>', desc('New Tab', false) },
+        ['<leader>tn'] = { ':tabN<CR>', desc('Next Tab', false) },
+        ['<leader>tp'] = { ':tabp<CR>', desc('Previous Tab', false) },
 
-        ['<leader>bn'] = { ':bNext<CR>', desc('Next Buffer', false, nil, false) },
-        ['<leader>bp'] = { ':bprevious<CR>', desc('Previous Buffer', false, nil, false) },
-        ['<leader>bd'] = { ':bdel<CR>', desc('Close Buffer', false, nil, false) },
         ['<leader>bD'] = { ':bdel!<CR>', desc('Close Buffer Forcefully', false, nil, false) },
+        ['<leader>bd'] = { ':bdel<CR>', desc('Close Buffer', false, nil, false) },
         ['<leader>bf'] = { ':bfirst<CR>', desc('Goto First Buffer', false, nil, false) },
         ['<leader>bl'] = { ':blast<CR>', desc('Goto Last Buffer', false, nil, false) },
+        ['<leader>bn'] = { ':bNext<CR>', desc('Next Buffer', false, nil, false) },
+        ['<leader>bp'] = { ':bprevious<CR>', desc('Previous Buffer', false, nil, false) },
     },
     v = {
-        ['<leader>s'] = { ':sort<CR>', desc('Sort Selection') },
         ['<leader>S'] = { ':sort!<CR>', desc('Sort Selection (Reverse)') },
+        ['<leader>s'] = { ':sort<CR>', desc('Sort Selection') },
 
-        ['<leader>f'] = { ':foldopen<CR>', desc('Open Fold') },
         ['<leader>F'] = { ':foldclose<CR>', desc('Close Fold') },
+        ['<leader>f'] = { ':foldopen<CR>', desc('Open Fold') },
+
+        ['<leader>ir'] = { ':retab<CR>', desc('Retab Selection') },
 
         ['<leader>r'] = { ':s/', desc('Run Search-Replace Prompt For Selection', false) },
-        ['<leader>ir'] = { ':retab<CR>', desc('Retab Selection') },
     },
 }
 --- `which-key` map group prefixes
 ---@type table<MapModes, RegKeysNamed>
 local Names = {
     n = {
-        --- File Handling
-        ['<leader>f'] = { name = '+File' },
-        --- Script File Handling
-        ['<leader>fv'] = { name = '+Script Files' },
-        --- Indent Control
-        ['<leader>fi'] = { name = '+Indent' },
-        --- Folding Control
-        ['<leader>fF'] = { name = '+Folding' },
-
-        --- Tabs Handling
-        ['<leader>t'] = { name = '+Tabs' },
-
         --- Buffer Handling
         ['<leader>b'] = { name = '+Buffer', noremap = false },
 
-        --- Window Handling
-        ['<leader>w'] = { name = '+Window' },
-        --- Window Splitting
-        ['<leader>ws'] = { name = '+Split' },
+        --- File Handling
+        ['<leader>f'] = { name = '+File' },
+        --- Folding Control
+        ['<leader>fF'] = { name = '+Folding' },
+        --- Indent Control
+        ['<leader>fi'] = { name = '+Indent' },
+        --- Script File Handling
+        ['<leader>fv'] = { name = '+Script Files' },
+
+        --- Help
+        ['<leader>h'] = { name = '+Help' },
 
         --- Exiting
         ['<leader>q'] = { name = '+Quit Nvim' },
 
-        --- Help
-        ['<leader>h'] = { name = '+Help' },
+        --- Tabs Handling
+        ['<leader>t'] = { name = '+Tabs' },
 
         --- Vim
         ['<leader>v'] = { name = '+Vim' },
         --- `init.lua` Editing
         ['<leader>ve'] = { name = '+Edit $MYVIMRC' },
+
+        --- Window Handling
+        ['<leader>w'] = { name = '+Window' },
+        --- Window Splitting
+        ['<leader>ws'] = { name = '+Split' },
     },
     v = {
+        --- Help
+        ['<leader>h'] = { name = '+Help' },
+
         --- Indent Control
         ['<leader>i'] = { name = '+Indent' },
 
         --- Vim
         ['<leader>v'] = { name = '+Vim' },
-
-        --- Help
-        ['<leader>h'] = { name = '+Help' },
     },
 }
 
@@ -308,7 +309,7 @@ if is_tbl(Pkg.colorschemes) and not empty(Pkg.colorschemes) then
             WK.register(NamesCsc[mode], { mode = mode })
         end
 
-        User.maps.map_dict(CscKeys, 'wk.register', true, mode)
+        Maps.map_dict(CscKeys, 'wk.register', true, mode)
     end
 
     if not empty(found_csc) then
