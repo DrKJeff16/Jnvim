4eb71171996ae85cec71a3388154165405d59911
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-06-19 08:01:25 -0600

    chore(init): Switch to `nightfox` colorscheme.
    Note to self: I really need to "gitignore" default colorscheme changes
    somehow...

--- init.lua
+++ init.lua
@@ -249,9 +249,9 @@ if is_tbl(Pkg.colorschemes) and not empty(Pkg.colorschemes) then
 
     --- Reorder to your liking.
     local selected = {
+        'nightfox',
         'catppuccin',
         'tokyonight',
-        'nightfox',
         'onedark',
         'spacemacs',
         'molokai',
