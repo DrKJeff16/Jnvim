a2fec9bf074618813361261c21d8cb92214bec37
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-06-21 11:20:58 -0600

    docs(api): Added docstrings to `check` code.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/user/check/value.lua
+++ lua/user/check/value.lua
@@ -9,6 +9,20 @@ require('user.types.user.check')
 local M = {
     -- NOTE: We define `is_nil` first as it's used by the other checkers.
 
+    --- Checks whether a value is `nil`, i.e. non existant or explicitly set as nil.
+    --- ## Parameters
+    ---
+    --- * `var`: Any data type to be checked if it's nil.
+    ---          **Keep in mind that if `multiple` is set to `true`, this _MUST_ be a _non-empty_ table**.
+    ---          Otherwise it will be flagged as non-existant and the function will return `true`.
+    ---
+    --- * `multiple`: Tell the function you're checking for multiple values. (Default: `false`).
+    ---               If set to `true`, every element of the table will be checked.
+    ---               If **any** element doesn't exist or is `nil`, the function automatically returns false.
+    ---
+    --- ## Return
+    --- A boolean value indicating whether the data is `nil` or doesn't exist.
+    --- ---
     is_nil = function(var, multiple)
         multiple = (multiple ~= nil and type(multiple) == 'boolean') and multiple or false
 
@@ -29,14 +43,6 @@ local M = {
 
         return true
     end,
-}
-
-local type_funcs = {
-    ['is_str'] = 'string',
-    ['is_bool'] = 'boolean',
-    ['is_fun'] = 'function',
-    ['is_num'] = 'number',
-    ['is_tbl'] = 'table',
 }
 
 ---@type fun(t: Types): ValueFunc
@@ -65,11 +71,98 @@ local function type_fun(t)
     end
 end
 
---- Create the rest of `is_*` functions, save for `is_int`
-for k, v in next, type_funcs do
-    M[k] = type_fun(v)
-end
+--- Checks whether a value is a string.
+--- ## Parameters
+---
+--- * `var`: Any data type to be checked if it's a string.
+---          **Keep in mind that if `multiple` is set to `true`, this _MUST_ be a _non-empty_ table**.
+---          Otherwise it will be flagged as a non-string and the function will return `false`.
+---
+--- * `multiple`: Tell the function you're checking for multiple values. (Default: `false`).
+---               If set to `true`, every element of the table will be checked.
+---               If **any** element is not a string, the function automatically returns false.
+---
+--- ## Return
+--- A boolean value indicating whether the data is a string or not.
+--- ---
+M.is_str = type_fun('string')
+--- Checks whether a value is a boolean.
+--- ## Parameters
+---
+--- * `var`: Any data type to be checked if it's a boolean.
+---          **Keep in mind that if `multiple` is set to `true`, this _MUST_ be a _non-empty_ table**.
+---          Otherwise it will be flagged as a non-boolean and the function will return `false`.
+---
+--- * `multiple`: Tell the function you're checking for multiple values. (Default: `false`).
+---               If set to `true`, every element of the table will be checked.
+---               If **any** element is not a boolean, the function automatically returns false.
+---
+--- ## Return
+--- A boolean value indicating whether the data is a boolean or not.
+--- ---
+M.is_bool = type_fun('boolean')
+--- Checks whether a value is a function.
+--- ## Parameters
+---
+--- * `var`: Any data type to be checked if it's a function.
+---          **Keep in mind that if `multiple` is set to `true`, this _MUST_ be a _non-empty_ table**.
+---          Otherwise it will be flagged as a non-function and the function will return `false`.
+---
+--- * `multiple`: Tell the function you're checking for multiple values. (Default: `false`).
+---               If set to `true`, every element of the table will be checked.
+---               If **any** element is not a function, the function automatically returns false.
+---
+--- ## Return
+--- A boolean value indicating whether the data is a function or not.
+--- ---
+M.is_fun = type_fun('function')
+--- Checks whether a value is a number.
+--- ## Parameters
+---
+--- * `var`: Any data type to be checked if it's a number.
+---          **Keep in mind that if `multiple` is set to `true`, this _MUST_ be a _non-empty_ table**.
+---          Otherwise it will be flagged as a non-number and the function will return `false`.
+---
+--- * `multiple`: Tell the function you're checking for multiple values. (Default: `false`).
+---               If set to `true`, every element of the table will be checked.
+---               If **any** element is not a number, the function automatically returns false.
+---
+--- ## Return
+--- A boolean value indicating whether the data is a number or not.
+--- ---
+M.is_num = type_fun('number')
+--- Checks whether a value is a table.
+--- ## Parameters
+---
+--- * `var`: Any data type to be checked if it's a table.
+---          **Keep in mind that if `multiple` is set to `true`, this _MUST_ be a _non-empty_ table**.
+---          Otherwise it will be flagged as a non-table and the function will return `false`.
+---
+--- * `multiple`: Tell the function you're checking for multiple values. (Default: `false`).
+---               If set to `true`, every element of the table will be checked.
+---               If **any** element is not a table, the function automatically returns false.
+---
+--- ## Return
+--- A boolean value indicating whether the data is a table or not.
+--- ---
+M.is_tbl = type_fun('table')
 
+--- Create the rest of `is_*` functions, save for `is_int`
+
+--- Checks whether a value is an integer i.e. _greater than or equal to `0` and a **whole number**_.
+--- ## Parameters
+---
+--- * `var`: Any data type to be checked if it's an integer.
+---          **Keep in mind that if `multiple` is set to `true`, this _MUST_ be a _non-empty_ table**.
+---          Otherwise it will be flagged as a non-integer and the function will return `false`.
+---
+--- * `multiple`: Tell the integer you're checking for multiple values. (Default: `false`).
+---               If set to `true`, every element of the table will be checked.
+---               If **any** element is not an integer, the function automatically returns false.
+---
+--- ## Return
+--- A boolean value indicating whether the data is an integer or not.
+--- ---
 function M.is_int(var, multiple)
     local is_nil = M.is_nil
     local is_tbl = M.is_tbl
@@ -95,14 +188,17 @@ function M.is_int(var, multiple)
     return true
 end
 
---- Returns whether a data value is "empty", including these scenarios:
+--- Returns whether a given string/number/table is "empty", including these scenarios:
 --- * Is an empty string (`x == ''`)
 --- * Is an integer equal to zero (`x == 0`)
 --- * Is an empty table
---- ---
+---
 --- ## Parameters
 --- * `v`: Must be either a string, number or a table.
 ---        Otherwise you'll get complaints and the function will return `true`
+---
+--- ## Returns
+--- A boolean indicatin whether input data is empty or not.
 --- ---
 function M.empty(v)
     local is_str = M.is_str
