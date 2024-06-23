c6eb325f6ed4e16ffa224f2a1636ff531db5efce
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-06-23 17:23:10 -0600

    feat(lazygit): Added `lazygit` plugin.

---
+++ lua/lazy_cfg/lazygit/init.lua
@@ -0,0 +1,143 @@
+---@diagnostic disable:unused-local
+---@diagnostic disable:unused-function
+
+local User = require('user')
+local Check = User.check
+local Maps = User.maps
+local kmap = Maps.kmap
+local WK = Maps.wk
+
+local exists = Check.exists.module
+local executable = Check.exists.executable
+local desc = kmap.desc
+local map_dict = Maps.map_dict
+
+if not (exists('lazygit.utils') and executable({ 'git', 'lazygit' })) then
+    return
+end
+
+local au = vim.api.nvim_create_autocmd
+
+local LG_Utils = require('lazygit.utils')
+local LG_Win = require('lazygit.window')
+
+local Opts = {
+    ['floating_window_winblend'] = 0,
+    ['floating_window_scaling_factor'] = 0.9,
+    ['floating_window_border_chars'] = { '╭', '─', '╮', '│', '╯', '─', '╰', '│' },
+    ['floating_window_use_plenary'] = 1,
+
+    ['use_neovim_remote'] = 0,
+
+    ['use_custom_config_file_path'] = 0,
+}
+
+for k, v in next, Opts do
+    vim.g['lazygit_' .. k] = v
+end
+
+---@type table<MapModes, KeyMapDict>
+local Keys = {
+    n = {
+        ['<leader>GlC'] = {
+            function()
+                vim.cmd('LazyGitConfig')
+            end,
+            desc("LazyGit's Config"),
+        },
+        ['<leader>GlF'] = {
+            function()
+                vim.cmd('LazyGitFilter')
+            end,
+            desc('Open Project Commits In Float'),
+        },
+        ['<leader>Glc'] = {
+            function()
+                vim.cmd('LazyGitCurrentFile')
+            end,
+            desc('LazyGit On Current File'),
+        },
+        ['<leader>Glf'] = {
+            function()
+                vim.cmd('LazyGitFilterCurrentFile')
+            end,
+            desc("LazyGit's Config"),
+        },
+        ['<leader>Glg'] = {
+            function()
+                vim.cmd('LazyGit')
+            end,
+            desc('Run LazyGit'),
+        },
+    },
+    v = {
+        ['<leader>GlC'] = {
+            function()
+                vim.cmd('LazyGitConfig')
+            end,
+            desc("LazyGit's Config"),
+        },
+        ['<leader>GlF'] = {
+            function()
+                vim.cmd('LazyGitFilter')
+            end,
+            desc('Open Project Commits In Float'),
+        },
+        ['<leader>Glc'] = {
+            function()
+                vim.cmd('LazyGitCurrentFile')
+            end,
+            desc('LazyGit On Current File'),
+        },
+        ['<leader>Glf'] = {
+            function()
+                vim.cmd('LazyGitFilterCurrentFile')
+            end,
+            desc("LazyGit's Config"),
+        },
+        ['<leader>Glg'] = {
+            function()
+                vim.cmd('LazyGit')
+            end,
+            desc('Run LazyGit'),
+        },
+    },
+}
+
+---@type table<MapModes, RegKeysNamed>
+local Names = {
+    n = {
+        ['<leader>G'] = { name = '+Git' },
+        ['<leader>Gl'] = { name = '+LazyGit' },
+    },
+    v = {
+        ['<leader>G'] = { name = '+Git' },
+        ['<leader>Gl'] = { name = '+LazyGit' },
+    },
+}
+
+if WK.available() then
+    map_dict(Names, 'wk.register', true, nil, 0)
+end
+map_dict(Keys, 'wk.register', true, nil, 0)
+
+au('BufEnter', {
+    pattern = '*',
+    callback = function()
+        require('lazygit.utils').project_root_dir()
+    end,
+})
+--[[ au('TermClose', {
+    pattern = '*',
+    function()
+        if require('user.util').ft_get() ~= 'lazygit' and not vim.v.event['status'] then
+            vim.fn.execute('bdelete! ' .. vim.fn.expand('<abuf>'), 'silent!')
+        end
+    end,
+}) ]]
+
+vim.cmd([[
+" NOTE: added lazygit check to avoid lua error
+" NOTE: added "silent!" to avoid error when FZF terminal window is closed.
+autocmd TermClose * if &filetype != 'lazygit' && !v:event.status | silent! exe 'bdelete! '..expand('<abuf>') | endif
+]])
