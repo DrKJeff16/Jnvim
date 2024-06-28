81c8a22ab12547edb7f9d5c66c702765bb6dbce1
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-06-27 22:00:25 -0600

    fix(ibl): Update and extended config.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/lazy_cfg/blank_line/init.lua
+++ lua/lazy_cfg/blank_line/init.lua
@@ -8,6 +8,7 @@ local hl_t = User.types.user.highlight
 local exists = Check.exists.module
 local is_str = Check.value.is_str
 local is_tbl = Check.value.is_tbl
+local is_int = Check.value.is_int
 local empty = Check.value.empty
 local hi = User.highlight.hl
 
@@ -35,7 +36,10 @@ local Hilite = {
 }
 
 ---@type string[]
-local highlight = {}
+local highlight = {
+    'Function',
+    'Label',
+}
 for k, _ in next, Hilite do
     if is_str(k) then
         table.insert(highlight, k)
@@ -62,21 +66,74 @@ local function apply_Hilite()
     end
 end
 
-register(HType.HIGHLIGHT_SETUP, apply_Hilite)
-register(HType.SCOPE_HIGHLIGHT, Builtin.scope_highlight_from_extmark)
+---@param bufnr? integer
+---@return boolean
+local function line_cond(bufnr)
+    return vim.api.nvim_buf_line_count(is_int(bufnr) and bufnr or 0) < 5000
+end
 
-Ibl.setup({
-    indent = {
-        highlight = highlight,
-        char = '•',
-    },
-    whitespace = {
-        highlight = highlight,
-        remove_blankline_trail = true,
-    },
-    scope = { enabled = true },
-})
+local function linebreak_check()
+    local vim_has = User.check.exists.vim_has
+
+    return vim_has('nvim-0.10') and vim.opt.breakindent and vim.opt.breakindentopt:get() ~= ''
+end
 
 if exists('rainbow-delimiters.setup') then
     vim.g.rainbow_delimiters = { highlight = names }
 end
+
+register(HType.ACTIVE, line_cond)
+register(HType.HIGHLIGHT_SETUP, apply_Hilite)
+register(HType.SCOPE_HIGHLIGHT, Builtin.scope_highlight_from_extmark)
+register(HType.SKIP_LINE, Hooks.builtin.skip_preproc_lines, { bufnr = 0 })
+register(HType.WHITESPACE, Hooks.builtin.hide_first_space_indent_level)
+register(HType.WHITESPACE, Hooks.builtin.hide_first_tab_indent_level)
+
+Ibl.setup({
+    enabled = true,
+    debounce = 200,
+    indent = {
+        highlight = highlight,
+        -- char = '•',
+        char = {
+            '╎',
+            '╏',
+            '┆',
+            '┇',
+            '┊',
+            '┋',
+        },
+        tab_char = {
+            '▏',
+            '▎',
+            '▍',
+            '▌',
+            '▋',
+            '▊',
+            '▉',
+            '█',
+        },
+
+        repeat_linebreak = linebreak_check(),
+
+        smart_indent_cap = true,
+    },
+    whitespace = {
+        highlight = { 'Whitespace', 'NonText' },
+        remove_blankline_trail = true,
+    },
+    scope = {
+        enabled = true,
+        show_end = true,
+        show_start = true,
+        show_exact_scope = true,
+        injected_languages = exists('nvim-treesitter'),
+        priority = 1024,
+        include = {
+            node_type = {
+                ['*'] = { '*' },
+                ['lua'] = { 'return_statement', 'table_constructor' },
+            },
+        },
+    },
+})
