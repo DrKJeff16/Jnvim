d3f55ead7eb88300ce039bdbcdc1549dcfc4dd07
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-07-06 13:31:31 -0600

    misc(lazy): Add `git` field for config.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -46,6 +46,18 @@ Lazy.setup({
         { import = 'plugin._spec' },
     },
 
+    git = {
+        -- defaults for the `Lazy log` command
+        -- log = { "--since=3 days ago" }, -- show commits from the last 3 days
+        log = { '-8' }, -- show the last 8 commits
+        timeout = 120, -- kill processes that take more than 2 minutes
+        url_format = 'https://github.com/%s.git',
+        -- lazy.nvim requires git >=2.19.0. If you really want to use lazy with an older version,
+        -- then set the below to false. This should work, but is NOT supported and will
+        -- increase downloads a lot.
+        filter = true,
+    },
+
     root = plugin_root,
 
     performance = {
@@ -85,6 +97,7 @@ Lazy.setup({
             return S
         end)(),
     },
+
     dev = {
         path = '~/Projects/nvim',
         patterns = {},
