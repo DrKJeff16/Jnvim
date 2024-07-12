52d19325e5c1d56e2c28c94a094da542f99fddd9
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-07-12 10:44:53 -0600

    fix(gitsigns): Lowered sign priority.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/gitsigns/init.lua
+++ lua/plugin/gitsigns/init.lua
@@ -151,7 +151,7 @@ GS.setup({
         virt_text_priority = 3,
     },
     current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
-    sign_priority = 6,
+    sign_priority = 4,
     update_debounce = 100,
     max_file_length = 40000, -- Disable if file is longer than this (in lines)
     status_formatter = nil,
