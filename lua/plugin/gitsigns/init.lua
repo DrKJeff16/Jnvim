4a03bbde3fa96d305ec5430e222bdc3642d474c7
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-07-18 03:32:49 -0600

    fix(gitsigns)!: Disable linehl due to color interference.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/gitsigns/init.lua
+++ lua/plugin/gitsigns/init.lua
@@ -129,7 +129,7 @@ GS.setup({
 
     signcolumn = vim.opt.signcolumn:get() == 'yes', -- Toggle with `:Gitsigns toggle_signs`
     numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
-    linehl = true, -- Toggle with `:Gitsigns toggle_linehl`
+    linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
     word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
     watch_gitdir = { follow_files = true },
     auto_attach = true,
