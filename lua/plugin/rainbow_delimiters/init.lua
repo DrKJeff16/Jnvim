6b75481e11d472dcc62cd080f57f5ae130d1c543
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-08-24 04:32:18 -0600

    refactor(rainbow_delimiters): Made config more efficient.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/rainbow_delimiters/init.lua
+++ lua/plugin/rainbow_delimiters/init.lua
@@ -2,6 +2,7 @@ local User = require('user_api')
 local Check = User.check
 
 local exists = Check.exists.module
+local is_str = Check.value.is_str
 local hl_from_dict = User.highlight.hl_from_dict
 
 if not exists('rainbow-delimiters') then
@@ -12,20 +13,35 @@ User.register_plugin('plugin.rainbow_delimiters')
 
 local RD = require('rainbow-delimiters')
 
----@type rainbow_delimiters.config
+---@type rainbow_delimiters.config.queries
+local QRY = {
+    [''] = 'rainbow-delimiters',
+    bash = 'rainbow-blocks',
+    html = 'rainbow-blocks',
+    markdown = 'rainbow-blocks',
+    latex = 'rainbow-blocks',
+    lua = 'rainbow-blocks',
+    python = 'rainbow-blocks',
+    sh = 'rainbow-blocks',
+}
+
+---@param s 'local'|'global'|'noop'
+---@return rainbow_delimiters.strategy
+local function strat(s)
+    s = (is_str(s) and vim.tbl_contains({ 'global', 'local', 'noop' }, s)) and s or 'global'
+
+    return require('rainbow-delimiters').strategy[s]
+end
+
 require('rainbow-delimiters.setup').setup({
     strategy = {
-        [''] = RD.strategy['global'],
-        commonlisp = RD.strategy['local'],
-        html = RD.strategy['local'],
-        vim = RD.strategy['local'],
+        [''] = strat('global'),
+        commonlisp = strat('local'),
+        html = strat('local'),
+        markdown = strat('local'),
+        vim = strat('local'),
     },
-    query = {
-        [''] = 'rainbow-delimiters',
-        html = 'rainbow-blocks',
-        latex = 'rainbow-blocks',
-        lua = 'rainbow-blocks',
-    },
+    query = QRY,
     priority = {
         [''] = 210,
     },
@@ -38,7 +54,7 @@ require('rainbow-delimiters.setup').setup({
         'RainbowDelimiterViolet',
         'RainbowDelimiterCyan',
     },
-    blacklist = {},
+    blacklist = { 'c', 'cpp' },
 })
 
 ---@type HlDict
