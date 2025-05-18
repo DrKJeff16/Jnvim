a5f665aca2699024acc0bc1d77134ab78771323c
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-05-18 16:30:12 -0600

    fix(lazy): Tightened dependencies and invokations for `cmp` plugins
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/_spec/completion.lua
+++ lua/plugin/_spec/completion.lua
@@ -9,13 +9,9 @@ local executable = User.check.exists.executable
 ---@type (LazySpec)[]
 local M = {
     {
-        'hrsh7th/nvim-cmp',
+        'hrsh7th/vim-vsnip',
         event = { 'InsertEnter', 'CmdlineEnter' },
         version = false,
-        init = function()
-            vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'noselect', 'preview' }
-        end,
-        config = source('plugin.cmp'),
     },
     {
         'onsails/lspkind.nvim',
@@ -23,57 +19,73 @@ local M = {
         version = false,
     },
     {
-        'hrsh7th/vim-vsnip',
+        'hrsh7th/nvim-cmp',
+        event = { 'InsertEnter', 'CmdlineEnter' },
         version = false,
+        dependencies = { 'vim-vsnip' },
+        init = function()
+            vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'noselect', 'preview' }
+        end,
+        config = source('plugin.cmp'),
     },
     {
         'hrsh7th/cmp-nvim-lsp',
         version = false,
-        dependencies = { 'neovim/nvim-lspconfig' },
+        dependencies = { 'neovim/nvim-lspconfig', 'nvim-cmp' },
     },
     {
         'hrsh7th/cmp-nvim-lsp-document-symbol',
         version = false,
-        dependencies = { 'neovim/nvim-lspconfig' },
+        dependencies = { 'neovim/nvim-lspconfig', 'nvim-cmp' },
     },
     {
         'hrsh7th/cmp-nvim-lsp-signature-help',
         version = false,
-        dependencies = { 'neovim/nvim-lspconfig' },
+        dependencies = { 'neovim/nvim-lspconfig', 'nvim-cmp' },
     },
     {
         'hrsh7th/cmp-buffer',
+        event = { 'InsertEnter', 'CmdlineEnter' },
         version = false,
+        dependencies = { 'nvim-cmp' },
     },
     {
         'hrsh7th/cmp-path',
+        event = { 'InsertEnter', 'CmdlineEnter' },
         version = false,
+        dependencies = { 'nvim-cmp' },
     },
     {
         'https://codeberg.org/FelipeLema/cmp-async-path',
+        event = { 'InsertEnter', 'CmdlineEnter' },
         version = false,
+        dependencies = { 'nvim-cmp' },
     },
     {
         'petertriho/cmp-git',
+        event = { 'InsertEnter', 'CmdlineEnter' },
         version = false,
+        dependencies = { 'nvim-cmp' },
         cond = executable('git'),
     },
     {
         'davidsierradz/cmp-conventionalcommits',
         ft = 'gitcommit',
         version = false,
+        dependencies = { 'nvim-cmp' },
         cond = executable('git'),
     },
     {
         'hrsh7th/cmp-cmdline',
         event = 'CmdlineEnter',
         version = false,
+        dependencies = { 'nvim-cmp' },
     },
     {
         'paopaol/cmp-doxygen',
         ft = 'doxygen',
         version = false,
-        dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
+        dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects', 'nvim-cmp' },
         cond = executable('doxygen'),
     },
     {
