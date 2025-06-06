93a61ddfafc3b006a6add0f804702c5674ef3724
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-06-06 13:02:24 -0600

    feat(api): Fix `user.commands`, new method, updated annotations.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/user_api/commands.lua
+++ lua/user_api/commands.lua
@@ -1,8 +1,17 @@
 ---@diagnostic disable:missing-fields
 
----@module 'user_api.types.user.commands')
+---@module 'user_api.types.user.commands'
+
+local Value = require('user_api.check.value')
 
 local desc = require('user_api.maps.kmap').desc
+local is_nil = Value.is_nil
+local is_str = Value.is_str
+local empty = Value.empty
+
+local new_cmd = vim.api.nvim_create_user_command
+local exec2 = vim.api.nvim_exec2
+local set_lines = vim.api.nvim_buf_set_lines
 
 ---@type User.Commands
 local Commands = {}
@@ -12,23 +21,17 @@ Commands.commands = {}
 
 Commands.commands.Redir = {
     function(ctx)
-        local set_lines = vim.api.nvim_buf_set_lines
-        local exec2 = vim.api.nvim_exec2
-
         local lines = vim.split(
-            exec2(ctx.args, {
-                output = true,
-            })['output'],
+            exec2(ctx.args, { output = true })['output'],
             newline or string.char(10), -- `'\n'`
             { plain = true }
         )
 
         local buf = vim.api.nvim_create_buf(true, true)
-        local win = vim.api.nvim_open_win(buf, true, {
-            vertical = false,
-        })
+        local win = vim.api.nvim_open_win(buf, true, { vertical = false })
 
         set_lines(buf, 0, -1, false, lines)
+
         vim.api.nvim_set_option_value('modified', false, { buf = buf })
     end,
     { nargs = '+', complete = 'command' },
@@ -42,11 +45,30 @@ Commands.commands.Redir = {
 }
 
 ---@param self User.Commands
-function Commands:setup()
-    local new_cmd = vim.api.nvim_create_user_command
+---@param name string
+---@param C User.Commands.CtxSpec|table
+function Commands:new_command(name, C)
+    if is_nil({ name, C }, true) or empty({ name, C }, true) then
+        error('(user_api.commands:new_command): nil/empty argument(s)')
+    end
 
+    if not is_str(name) then
+        error('(user_api.commands:new_command): Invalid command name')
+    end
+
+    self.commands[name] = C
+
+    self:setup()
+end
+
+---@param self User.Commands
+function Commands:setup()
     for cmd, T in next, self.commands do
-        new_cmd(cmd, T[1], T[2] or {})
+        local ok, _ = pcall(new_cmd, cmd, T[1], T[2] or {})
+
+        if not ok then
+            vim.notify('Bad command: `' .. cmd .. '`', vim.log.levels.WARN)
+        end
     end
 
     vim.g.user_api_commans_setup = 1
@@ -54,11 +76,8 @@ end
 
 ---@param self User.Commands
 function Commands:setup_keys()
-    local Value = require('user_api.check.value')
-    local is_nil = Value.is_nil
     local is_int = Value.is_int
     local is_tbl = Value.is_tbl
-    local empty = Value.empty
 
     local abort = not (is_int(vim.g.user_api_commans_setup) and vim.g.user_api_commans_setup == 1)
 
