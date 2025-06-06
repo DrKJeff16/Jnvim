26d54a9f65235daf756a708d0728e1d7ebc31a60
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-06-05 22:48:57 -0600

    feat(lsp): Added `vimls` to LSP calls.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/lsp/server_config.lua
+++ lua/plugin/lsp/server_config.lua
@@ -294,6 +294,31 @@ Clients.pylsp = {
     },
 }
 
+Clients.vimls = {
+    cmd = { 'vim-language-server', '--stdio' },
+    filetypes = { 'vim' },
+    init_options = {
+        diagnostic = {
+            enable = true,
+        },
+        indexes = {
+            count = 3,
+            gap = 100,
+            projectRootPatterns = { 'runtime', 'nvim', '.git', 'autoload', 'plugin' },
+            runtimepath = true,
+        },
+        isNeovim = true,
+        iskeyword = '@,48-57,_,192-255,-#',
+        runtimepath = '',
+        suggest = {
+            fromRuntimepath = true,
+            fromVimruntime = true,
+        },
+        vimruntime = '',
+    },
+    root_markers = { '.git' },
+}
+
 return Clients
 
 --- vim:ts=4:sts=4:sw=4:et:ai:si:sta:noci:nopi:
