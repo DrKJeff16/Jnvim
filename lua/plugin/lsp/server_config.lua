899bbfdcc3ec90588bfab092f21c21c1bc03f00a
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-06-06 23:23:18 -0600

    feat(lsp): Added `jsonls` to LSP configs.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/lsp/server_config.lua
+++ lua/plugin/lsp/server_config.lua
@@ -395,6 +395,15 @@ Clients.vimls = {
     root_markers = { '.git' },
 }
 
+Clients.jsonls = {
+    cmd = { 'vscode-json-language-server', '--stdio' },
+    filetypes = { 'json', 'jsonc' },
+    init_options = {
+        provideFormatter = true,
+    },
+    root_markers = { '.git' },
+}
+
 return Clients
 
 --- vim:ts=4:sts=4:sw=4:et:ai:si:sta:noci:nopi:
