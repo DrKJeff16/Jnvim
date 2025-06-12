9879e564485c402f0e30e5eca087a07c8b7d8a28
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-06-12 17:07:12 -0600

    feat(api): `User.update:update()` extebded, and keymap added.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/user_api/types/user/update.lua
+++ lua/user_api/types/user/update.lua
@@ -1,7 +1,7 @@
 ---@meta
 
 ---@class User.Update
----@field update fun(self: User.Update): string?
+---@field update fun(self: User.Update, verbose: boolean?): string?
 ---@field setup_maps fun(self: User.Update)
 
 --- vim:ts=4:sts=4:sw=4:et:ai:si:sta:noci:nopi:
--- lua/user_api/update.lua
+++ lua/user_api/update.lua
@@ -6,14 +6,18 @@
 local Update = {}
 
 ---@param self User.Update
+---@param verbose? boolean
 ---@return string?
-function Update:update()
+function Update:update(verbose)
     local notify = require('user_api.util.notify').notify
+    local is_bool = require('user_api.check.value').is_bool
 
     local curr_win = vim.api.nvim_get_current_win
     local curr_tab = vim.api.nvim_get_current_tabpage
 
-    local old_cwd = vim.fn.getcwd(curr_win(), curr_tab())
+    verbose = is_bool(verbose) and verbose or false
+
+    local og_cwd = vim.fn.getcwd(curr_win(), curr_tab())
 
     local cmd = {
         'git',
@@ -27,28 +31,26 @@ function Update:update()
     local res = vim.fn.system(cmd)
 
     if vim.v.shell_error ~= 0 then
-        notify('Failed to update Jnvim, try to do it manually...', 'error', {
-            animate = false,
-            hide_from_history = false,
-            timeout = 5000,
-            title = 'User API',
-        })
+        error('Failed to update Jnvim, try to do it manually...', vim.log.levels.ERROR)
     end
 
     if res:match('Already up to date') then
         notify('Jnvim is up to date!', 'info', {
             animate = true,
             hide_from_history = true,
-            timeout = 5000,
+            timeout = 2000,
             title = 'User API',
         })
     elseif not res:match('error') then
-        notify(res, 'debug', {
-            animate = true,
-            hide_from_history = true,
-            timeout = 2500,
-            title = 'User API',
-        })
+        if verbose then
+            notify(res, 'debug', {
+                animate = true,
+                hide_from_history = true,
+                timeout = 2250,
+                title = 'User API',
+            })
+        end
+
         notify('You need to restart Nvim!', 'warn', {
             animate = true,
             hide_from_history = false,
@@ -57,7 +59,7 @@ function Update:update()
         })
     end
 
-    vim.api.nvim_set_current_dir(old_cwd)
+    vim.api.nvim_set_current_dir(og_cwd)
 
     return res
 end
@@ -74,6 +76,10 @@ function Update:setup_maps()
             ['<leader>Uu'] = {
                 function() self:update() end,
                 desc('Update User Config'),
+            },
+            ['<leader>UU'] = {
+                function() self:update(true) end,
+                desc('Update User Config (Verbose)', false),
             },
         },
     })
