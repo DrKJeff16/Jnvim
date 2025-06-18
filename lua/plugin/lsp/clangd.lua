1920521ca8a2db4eca75d2257e02cb8f42d79a53
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-06-17 22:30:26 -0600

    fixup(lsp)!: `clangd` was behaving very badly when used, so I updated its config.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/lsp/clangd.lua
+++ lua/plugin/lsp/clangd.lua
@@ -3,49 +3,14 @@ local Check = User.check
 
 local exists = Check.exists.module
 local executable = Check.exists.executable
-local vim_has = Check.exists.vim_has
 
 if not (executable('clangd') and exists('clangd_extensions')) then
     return
 end
 
-User:register_plugin('plugin.lsp.clangd')
+local Clangd = require('clangd_extensions')
 
-local Exts = require('clangd_extensions')
-local Inlay = require('clangd_extensions.inlay_hints')
-
-Exts.setup({
-    inlay_hints = {
-        inline = vim_has('nvim-0.10'),
-        -- Options other than `highlight' and `priority' only work
-        -- if `inline' is disabled
-        -- Only show inlay hints for the current line
-        only_current_line = true,
-        -- Event which triggers a refresh of the inlay hints.
-        -- You can make this { "CursorMoved" } or { "CursorMoved,CursorMovedI" } but
-        -- not that this may cause  higher CPU usage.
-        -- This option is only respected when only_current_line and
-        -- autoSetHints both are true
-        only_current_line_autocmd = { 'CursorHold', 'CursorHoldI', 'CursorMoved', 'CursorMovedI' },
-        -- whether to show parameter hints with the inlay hints or not
-        show_parameter_hints = false,
-        -- prefix for parameter hints
-        parameter_hints_prefix = '<- ',
-        -- prefix for all the other hints (type, chaining)
-        other_hints_prefix = '=> ',
-        -- whether to align to the length of the longest line in the file
-        max_len_align = false,
-        -- padding from the left if max_len_align is true
-        max_len_align_padding = 1,
-        -- whether to align to the extreme right or not
-        right_align = false,
-        -- padding from the right if right_align is true
-        right_align_padding = 7,
-        -- The color of the hints
-        highlight = 'Comment',
-        -- The highlight group priority for extmark
-        priority = 60,
-    },
+Clangd.setup({
     ast = {
         -- These require codicons (https://github.com/microsoft/vscode-codicons)
         role_icons = {
@@ -69,11 +34,10 @@ Exts.setup({
 
         highlights = { detail = 'Comment' },
     },
-    memory_usage = { border = 'rounded' },
-    symbol_info = { border = 'shadow' },
+    memory_usage = { border = 'double' },
+    symbol_info = { border = 'rounded' },
 })
 
-Inlay.setup_autocmd()
-Inlay.set_inlay_hints()
+User:register_plugin('plugin.lsp.clangd')
 
 --- vim:ts=4:sts=4:sw=4:et:ai:si:sta:noci:nopi:
