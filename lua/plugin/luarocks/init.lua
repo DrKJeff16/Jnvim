b8276a128d2675fc6102f1fb4a2224ea55adddad
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-06-28 13:53:51 -0600

    chore(luarocks): Moved API call to bottom.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/luarocks/init.lua
+++ lua/plugin/luarocks/init.lua
@@ -1,13 +1,10 @@
 local User = require('user_api')
-local Check = User.check
 
-local exists = Check.exists.module
+local exists = User.check.exists.module
 
 if not exists('luarocks-nvim') then
     return
 end
-
-User:register_plugin('plugin.luarocks')
 
 local Rocks = require('luarocks-nvim')
 
@@ -20,5 +17,7 @@ Rocks.setup({
     },
     luarocks_buird_args = { '--local' },
 })
+
+User:register_plugin('plugin.luarocks')
 
 --- vim:ts=4:sts=4:sw=4:et:ai:si:sta:noci:nopi:
