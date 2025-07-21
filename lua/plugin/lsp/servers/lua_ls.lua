0c47559171bd41351417ce852aca4472e4c01902
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-07-21 11:33:49 -0600

    chore(lsp): Refactored `on_init()` for `lua_ls`.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/lsp/servers/lua_ls.lua
+++ lua/plugin/lsp/servers/lua_ls.lua
@@ -1,12 +1,66 @@
 local User = require('user_api')
 
-local fs_stat = (vim.uv or vim.loop).fs_stat
 local extend = vim.tbl_deep_extend
+local copy = vim.deepcopy
 
-local library = { vim.api.nvim_get_runtime_file('', true) }
+---@param client vim.lsp.Client
+local function on_init(client)
+    if client.workspace_folders then
+        local path = client.workspace_folders[1].name
+        if path:sub(-5) ~= '/nvim' and path:sub(-5) ~= '.nvim' then
+            client.config.settings.Lua = extend('force', copy(client.config.settings.Lua), {
+                runtime = {
+                    path = {
+                        'lua/?.lua',
+                        'lua/?/init.lua',
+                        '?.lua',
+                        '?/?.lua',
+                        '?/?/init.lua',
+                        'init.lua',
+                    },
+                },
+                workspace = {
+                    checkThirdParty = false,
+                    useGitIgnore = true,
+                    library = { path .. '/lua' },
+                },
+            })
 
-table.insert(library, '${3rd}/luv/library')
-table.insert(library, '${3rd}/busted/library')
+            return
+        end
+    end
+
+    local library = { vim.api.nvim_get_runtime_file('', true) }
+    table.insert(library, '${3rd}/luv/library')
+    table.insert(library, '${3rd}/busted/library')
+
+    client.config.settings.Lua = extend('force', client.config.settings.Lua, {
+        diagnostics = {
+            enable = true,
+            globals = { 'vim' },
+        },
+        runtime = {
+            -- Tell the language server which version of Lua you're using (most
+            -- likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT',
+            -- Tell the language server how to find Lua modules same way as Neovim
+            -- (see `:h lua-module-load`)
+            path = {
+                'lua/?.lua',
+                'lua/?/init.lua',
+                '?.lua',
+                '?/?.lua',
+                '?/?/init.lua',
+                'init.lua',
+            },
+        },
+        workspace = {
+            checkThirdParty = false,
+            useGitIgnore = true,
+            library = library,
+        },
+    })
+end
 
 User:register_plugin('plugin.lsp.servers.lua_ls')
 
@@ -24,63 +78,7 @@ return {
         '.git',
     },
 
-    ---@param client vim.lsp.Client
-    on_init = function(client)
-        if client.workspace_folders then
-            local path = client.workspace_folders[1].name
-            if path:sub(-5) ~= '/nvim' and path:sub(-5) ~= '.nvim' then
-                client.config.settings.Lua = extend('force', client.config.settings.Lua, {
-                    runtime = {
-                        path = {
-                            'lua/?.lua',
-                            'lua/?/init.lua',
-                            '?.lua',
-                            '?/?.lua',
-                            '?/?/init.lua',
-                            'init.lua',
-                        },
-                    },
-                    workspace = {
-                        checkThirdParty = false,
-                        library = {
-                            path,
-                            path .. '/lua',
-                            path .. '/types',
-                        },
-                    },
-                })
-
-                return
-            end
-        end
-
-        client.config.settings.Lua = extend('force', client.config.settings.Lua, {
-            diagnostics = {
-                enable = true,
-                globals = { 'vim' },
-            },
-            runtime = {
-                -- Tell the language server which version of Lua you're using (most
-                -- likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-                -- Tell the language server how to find Lua modules same way as Neovim
-                -- (see `:h lua-module-load`)
-                path = {
-                    'lua/?.lua',
-                    'lua/?/init.lua',
-                    '?.lua',
-                    '?/?.lua',
-                    '?/?/init.lua',
-                    'init.lua',
-                },
-            },
-            workspace = {
-                checkThirdParty = false,
-                useGitIgnore = true,
-                library = library,
-            },
-        })
-    end,
+    on_init = on_init,
 
     settings = {
         Lua = {
