48e85c477177459e3dec0b56c5740654d2175630
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-07-23 21:45:42 -0600

    refactor(api): `user.config.neovide` rewriting.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/user_api/config/neovide.lua
+++ lua/user_api/config/neovide.lua
@@ -15,6 +15,115 @@ local function alpha()
     return string.format('%x', math.floor(255 * (vim.g.transparency or 1.0)))
 end
 
+---@class Config.Neovide.Opts.G
+local g_opts = {
+    theme = 'auto',
+
+    refresh_rate = 60,
+    refresh_rate_idle = 30,
+
+    no_idle = true,
+
+    ---@type boolean
+    confirm_quit = vim.opt.confirm:get(),
+
+    fullscreen = false,
+
+    profiler = false,
+
+    cursor = {
+        hack = false,
+        animation_length = 0.1,
+        short_animation_length = 0.05,
+
+        trail_size = 1.0,
+
+        antialiasing = false,
+
+        smooth = {
+            blink = true,
+        },
+
+        animate = {
+            in_insert_mode = true,
+            command_line = true,
+        },
+    },
+
+    underline = {
+        stroke_scale = 1.0,
+    },
+
+    experimental = {
+        layer_grouping = false,
+    },
+
+    text = {
+        contrast = 0.5,
+        gamma = 0.0,
+    },
+
+    scale_factor = 1.0,
+
+    show_border = true,
+
+    hide_mouse_when_typing = false,
+
+    position = {
+        animation = {
+            length = 0.15,
+        },
+    },
+
+    scroll = {
+        animation = {
+            length = 0.07,
+            far_lines = 0,
+        },
+    },
+
+    remember = {
+        window_size = true,
+    },
+
+    padding = {
+        top = 0,
+        bottom = 0,
+        left = 0,
+        right = 0,
+    },
+
+    floating = {
+        blur_amount_x = 2.0,
+        blur_amount_y = 2.0,
+
+        shadow = true,
+        z_height = 50,
+
+        corner_radius = 0.5,
+    },
+
+    light = {
+        angle_degrees = 45,
+        radius = 5,
+    },
+}
+
+---@class Config.Neovide.Opts.O
+local o_opts = {
+    guifont = 'FiraCode Nerd Font Mono:h19',
+}
+
+---@class Config.Neovide.Opts.Opt
+local opt_opts = {
+    linespace = 0,
+}
+
+---@class Config.Neovide.Opts
+---@field g Config.Neovide.Opts.G
+---@field opt Config.Neovide.Opts.Opt
+---@field o Config.Neovide.Opts.O
+
 ---@class User.Config.Neovide
 local Neovide = {}
 
@@ -22,120 +131,26 @@ Neovide.g_opts = {}
 
 Neovide.active = false
 
----@class Config.Neovide.Opts
-Neovide.default_opts = {
-    ---@class Config.Neovide.Opts.G
-    g = {
-        theme = 'auto',
-
-        refresh_rate = 60,
-        refresh_rate_idle = 60,
-
-        no_idle = true,
-
-        ---@type boolean
-        confirm_quit = vim.opt.confirm:get(),
-
-        fullscreen = false,
-
-        cursor = {
-            hack = true,
-            animation_length = 0.0,
-            short_animation_length = 0.0,
-
-            trail_size = 1.0,
-
-            antialiasing = true,
-
-            smooth = {
-                blink = true,
-            },
-
-            animate = {
-                in_insert_mode = true,
-                command_line = true,
-            },
-        },
-
-        underline = {
-            stroke_scale = 1.0,
-        },
-
-        experimental = {
-            layer_grouping = false,
-        },
-
-        text = {
-            contrast = 0.5,
-            gamma = 0.0,
-        },
-
-        scale_factor = 1.0,
-
-        show_border = true,
-
-        hide_mouse_when_typing = false,
-
-        position = {
-            animation = {
-                length = 0.15,
-            },
-        },
-
-        scroll = {
-            animation = {
-                length = 0.07,
-
-                ---@type integer
-                far_lines = vim.opt.scrolloff:get(),
-            },
-        },
-
-        padding = {
-            top = 0,
-            bottom = 0,
-            left = 0,
-            right = 0,
-        },
-
-        floating = {
-            blur_amount_x = 2.0,
-            blur_amount_y = 2.0,
-
-            shadow = true,
-            z_height = 50,
-
-            corner_radius = 0.2,
-        },
-
-        light = {
-            angle_degrees = 45,
-            radius = 5,
-        },
-    },
-
-    ---@class Config.Neovide.Opts.O
-    o = {
-        guifont = 'FiraCode Nerd Font Mono:h19',
-    },
-
-    ---@class Config.Neovide.Opts.Opt
-    opt = {
-        linespace = 0,
-    },
-}
+---@return Config.Neovide.Opts
+function Neovide.get_defaults()
+    return {
+        g = g_opts,
+        opt = opt_opts,
+        o = o_opts,
+    }
+end
 
 ---@return boolean
-function Neovide:check()
-    self.active = executable('neovide') and vim.g.neovide ~= nil
+function Neovide.check()
+    Neovide.active = executable('neovide') and vim.g.neovide ~= nil
 
-    return self.active
+    return Neovide.active
 end
 
 ---@param opacity? number
 ---@param transparency? number
 ---@param bg? string
-function Neovide:set_transparency(opacity, transparency, bg)
+function Neovide.set_transparency(opacity, transparency, bg)
     if not (is_num(opacity) and num_range(opacity, 0.0, 1.0, { high = true, low = true })) then
         opacity = 0.85
     end
@@ -161,28 +176,28 @@ function Neovide:set_transparency(opacity, transparency, bg)
         end
     end
 
-    self.g_opts.neovide_opacity = opacity
-    self.g_opts.transparency = transparency
-    self.g_opts.neovide_background_color = bg
+    Neovide.g_opts.neovide_opacity = opacity
+    Neovide.g_opts.transparency = transparency
+    Neovide.g_opts.neovide_background_color = bg
 end
 
 ---@param O any
 ---@param pfx string
-function Neovide:parse_g_opts(O, pfx)
+function Neovide.parse_g_opts(O, pfx)
     pfx = (is_str(pfx) and pfx:sub(1, 8) == 'neovide_') and pfx or 'neovide_'
 
     for k, v in next, O do
         local key = pfx .. k
         if is_tbl(v) then
-            self:parse_g_opts(v, key .. '_')
+            Neovide.parse_g_opts(v, key .. '_')
         else
-            self.g_opts[key] = v
+            Neovide.g_opts[key] = v
         end
     end
 end
 
-function Neovide:setup_keys()
-    if not self:check() then
+function Neovide.setup_keys()
+    if not Neovide.check() then
         return
     end
 
@@ -213,12 +228,12 @@ end
 ---@param T? table
 ---@param transparent? boolean
 ---@param verbose? boolean
-function Neovide:setup(T, transparent, verbose)
-    if not self:check() then
+function Neovide.setup(T, transparent, verbose)
+    if not Neovide.check() then
         return
     end
 
-    if not self.active then
+    if not Neovide.active then
         return
     end
 
@@ -226,33 +241,57 @@ function Neovide:setup(T, transparent, verbose)
     transparent = is_bool(transparent) and transparent or false
     verbose = is_bool(verbose) and verbose or false
 
-    for o, v in next, self.default_opts.opt do
+    local Defaults = Neovide.get_defaults()
+
+    for o, v in next, Defaults.opt do
         vim.opt[o] = v
     end
 
-    for o, v in next, self.default_opts.o do
+    for o, v in next, Defaults.o do
         vim.o[o] = v
     end
 
     ---@type table<string, any>
-    self.g_opts = {}
+    Neovide.g_opts = {}
 
-    T = vim.tbl_deep_extend('keep', T, self.default_opts.g)
-    self:parse_g_opts(T, 'neovide_')
+    T = vim.tbl_deep_extend('keep', T, Defaults.g)
+    Neovide.parse_g_opts(T, 'neovide_')
 
     if transparent then
-        self:set_transparency()
+        Neovide.set_transparency()
     end
 
-    for k, v in next, self.g_opts do
+    local function set_ime(args)
+        if args.event:match('Enter$') then
+            vim.g.neovide_input_ime = true
+        else
+            vim.g.neovide_input_ime = false
+        end
+    end
+
+    local ime_input = vim.api.nvim_create_augroup('ime_input', { clear = true })
+
+    vim.api.nvim_create_autocmd({ 'InsertEnter', 'InsertLeave' }, {
+        group = ime_input,
+        pattern = '*',
+        callback = set_ime,
+    })
+
+    vim.api.nvim_create_autocmd({ 'CmdlineEnter', 'CmdlineLeave' }, {
+        group = ime_input,
+        pattern = '[/\\?]',
+        callback = set_ime,
+    })
+
+    for k, v in next, Neovide.g_opts do
         vim.g[k] = v
     end
 
     if verbose then
-        vim.notify((inspect or vim.inspect)(self.g_opts), INFO)
+        vim.notify((inspect or vim.inspect)(Neovide.g_opts), INFO)
     end
 
-    self:setup_keys()
+    Neovide.setup_keys()
 end
 
 return Neovide
