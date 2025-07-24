16466c3c25df7ff569ffbc60145b6a0e56936376
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-07-23 21:46:59 -0600

    fix(lsp): Reverted to a simpler `lua_ls` config.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/lsp/servers/lua_ls.lua
+++ lua/plugin/lsp/servers/lua_ls.lua
@@ -2,39 +2,33 @@ local User = require('user_api')
 
 local extend = vim.tbl_deep_extend
 local copy = vim.deepcopy
+local fs_stat = (vim.uv or vim.loop).fs_stat
+
+local stdpath = vim.fn.stdpath('config')
 
 ---@param client vim.lsp.Client
 local function on_init(client)
-    if client.workspace_folders then
-        local path = client.workspace_folders[1].name
-        if path:sub(-5) ~= '/nvim' and path:sub(-5) ~= '.nvim' then
-            client.config.settings.Lua = extend('force', copy(client.config.settings.Lua), {
-                runtime = {
-                    path = {
-                        'lua/?.lua',
-                        'lua/?/init.lua',
-                        '?.lua',
-                        '?/?.lua',
-                        '?/?/init.lua',
-                        'init.lua',
-                    },
-                },
-                workspace = {
-                    checkThirdParty = false,
-                    useGitIgnore = true,
-                    library = { path .. '/lua' },
-                },
-            })
-
-            return
-        end
+    if not client.workspace_folders then
+        return
     end
 
-    local library = { vim.api.nvim_get_runtime_file('', true) }
+    local path = client.workspace_folders[1].name
+    local luarc = {
+        path .. '/luarc.json',
+        path .. '/.luarc.json',
+    }
+
+    if path ~= stdpath and (fs_stat(luarc[1]) or fs_stat(luarc[2])) then
+        return
+    end
+
+    local library = {
+        (unpack or table.unpack)(vim.api.nvim_get_runtime_file('', true)),
+    }
     table.insert(library, '${3rd}/luv/library')
     table.insert(library, '${3rd}/busted/library')
 
-    client.config.settings.Lua = extend('force', client.config.settings.Lua, {
+    client.config.settings.Lua = extend('force', copy(client.config.settings.Lua), {
         diagnostics = {
             enable = true,
             globals = { 'vim' },
@@ -46,12 +40,9 @@ local function on_init(client)
             -- Tell the language server how to find Lua modules same way as Neovim
             -- (see `:h lua-module-load`)
             path = {
+                '?.lua',
                 'lua/?.lua',
                 'lua/?/init.lua',
-                '?.lua',
-                '?/?.lua',
-                '?/?/init.lua',
-                'init.lua',
             },
         },
         workspace = {
@@ -129,12 +120,9 @@ return {
                 pathStrict = false,
                 unicodeName = false,
                 path = {
+                    '?.lua',
                     'lua/?.lua',
                     'lua/?/init.lua',
-                    '?.lua',
-                    '?/?.lua',
-                    '?/?/init.lua',
-                    'init.lua',
                 },
             },
             semantic = {
