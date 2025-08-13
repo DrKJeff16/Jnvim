183058f4e6b3d41e75b3c13d456b095f6acdc5bb
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-08-13 13:10:13 -0600

    fix(project)!: Dropped features now integrated upstream.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/project.lua
+++ lua/plugin/project.lua
@@ -1,15 +1,9 @@
 local Keymaps = require('user_api.config.keymaps')
 local User = require('user_api')
 local Check = User.check
-local Util = User.util
-local Commands = User.commands
 
 local exists = Check.exists.module
-local type_not_empty = Check.value.type_not_empty
-local is_str = Check.value.is_str
 local desc = User.maps.kmap.desc
-
-local fmt = string.format
 
 if not exists('project_nvim') then
     return
@@ -171,106 +165,17 @@ local Keys = {
         end,
         desc('Attempt to run `:checkhealth project_nvim`'),
     },
+    ['<leader>pC'] = {
+        vim.cmd.ProjectConfig,
+        desc('Print Project Config'),
+    },
+    ['<leader>pr'] = {
+        vim.cmd.ProjectRecents,
+        desc('Print Recent Projects'),
+    },
 }
 
 Keymaps({ n = Keys })
-
-Commands.setup({
-    ProjectRecent = {
-        function()
-            local curr, method = Project.current_project()
-
-            vim.notify(fmt('Current: `%s`\nMethod: `%s`', curr, method))
-        end,
-        {},
-        mappings = {
-            n = {
-                ['<leader>pr'] = {
-                    function()
-                        local notify = require('user_api.util.notify').notify
-
-                        local recent_proj = require('project_nvim').get_recent_projects()
-
-                        recent_proj = Util.reverse_tbl(vim.deepcopy(recent_proj))
-
-                        local len = #recent_proj
-                        local msg = ''
-
-                        if type_not_empty('table', recent_proj) then
-                            for k, v in next, recent_proj do
-                                msg = string.format('%s %s. "%s"', msg, tostring(k), v)
-
-                                if k < len then
-                                    msg = string.format('%s\n', msg)
-                                end
-                            end
-                            notify(msg, 'info', {
-                                title = 'Project | Recent Projects',
-                                animate = true,
-                                timeout = 3000,
-                                hide_from_history = false,
-                            })
-
-                            return
-                        end
-
-                        notify('{}', 'error', {
-                            title = 'Recent Projects | NO PROJECTS',
-                            animate = true,
-                            timeout = 2000,
-                            hide_from_history = false,
-                        })
-                    end,
-                    desc('Print Recent Projects'),
-                },
-            },
-        },
-    },
-
-    ProjectConfig = {
-        function()
-            notify_inspect(Project.get_config())
-        end,
-        {},
-        mappings = {
-            n = {
-                ['<leader>pC'] = {
-                    function()
-                        local notify = require('user_api.util.notify').notify
-
-                        local cfg = require('project_nvim').get_config()
-                        local msg = ''
-
-                        if type_not_empty('table', cfg) then
-                            for k, v in next, cfg do
-                                k = is_str(k) and k or tostring(k)
-                                v = is_str(v) and v or inspect(v)
-
-                                msg = string.format('%s %s = %s\n', msg, k, v)
-                            end
-                            notify(string.format('{\n%s\n}', msg), 'info', {
-                                title = 'Project | Config',
-                                animate = true,
-                                timeout = 3000,
-                                hide_from_history = false,
-                            })
-
-                            return
-                        end
-
-                        notify('{}', 'error', {
-                            title = 'Project | NO CONFIG',
-                            animate = true,
-                            timeout = 2000,
-                            hide_from_history = false,
-                        })
-                    end,
-                    desc('Print Project Config'),
-                },
-            },
-        },
-    },
-})
 
 User.register_plugin('plugin.project')
 
