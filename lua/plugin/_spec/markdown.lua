abead1b0c65a0e707e1ac331e8a3359ba0bdfb06
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-09-24 15:39:06 -0600

    fix(markdown): migrated config structure to lazy spec
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/_spec/markdown.lua
+++ lua/plugin/_spec/markdown.lua
@@ -1,25 +1,9 @@
 ---@module 'lazy'
 
-local CfgUtil = require('config.util')
-
 ---@type LazySpec[]
 local MD = {
-    {
-        'tadmccorkle/markdown.nvim',
-        ft = 'markdown',
-        version = false,
-        config = CfgUtil.require('plugin.markdown'),
-    },
-    {
-        'MeanderingProgrammer/render-markdown.nvim',
-        dependencies = {
-            'nvim-treesitter/nvim-treesitter',
-            'echasnovski/mini.nvim',
-            'nvim-tree/nvim-web-devicons',
-        },
-        config = CfgUtil.require('plugin.markdown.render'),
-        cond = not in_console(),
-    },
+    { import = 'plugin.markdown' },
+    { import = 'plugin.markdown.render' },
 }
 
 return MD
