f270e884d34a8807d9330b1cc9b1cc686b064d17
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-10-06 17:01:26 -0600

    fix(api): `user.util.notify` fixes
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/user_api/util/notify.lua
+++ lua/user_api/util/notify.lua
@@ -1,26 +1,13 @@
 ---@module 'notify'
 
----@alias VimNotifyLvl
----|0
----|1
----|2
----|3
----|4
----|5
-
----@alias NotifyLvl
----|'debug'
----|'error'
----|'info'
----|'off'
----|'trace'
----|'warn'
+---@alias VimNotifyLvl vim.log.levels
+---@alias NotifyLvl 'debug'|'error'|'info'|'off'|'trace'|'warn'
 
 ---@class NotifyOpts
 ---@field title? string Defaults to `'Message'`
 ---@field icon? string
 ---@field timeout? integer|boolean Defaults to `700`
----@field on_open? fun(...)
+---@field on_open? function
 ---@field on_close? fun(...)
 ---@field keep? fun(...)
 ---@field render? string|fun(...)
@@ -82,21 +69,24 @@ Notify.Levels = {
 ---@param msg string
 ---@param lvl? NotifyLvl|VimNotifyLvl
 ---@param opts? notify.Options
+---@return function
 function Notify.notify(msg, lvl, opts)
-    if type(msg) ~= 'string' then
-        error('(user_api.util.notify.notify): Message is not a string')
+    if vim.fn.has('nvim-0.11') == 1 then
+        vim.validate('msg', msg, 'string', false)
+        vim.validate('lvl', lvl, { 'string', 'number' }, true)
+        vim.validate('opts', opts, 'table', true)
+    else
+        vim.validate({
+            msg = { msg, 'string' },
+            lvl = { lvl, { 'string', 'number', 'nil' } },
+            opts = { opts, { 'table', 'nil' } },
+        })
     end
-    if msg == '' then
-        error('(user_api.util.notify.notify): Empty message')
-    end
-
+    lvl = lvl or 'info'
     lvl = (not vim.tbl_contains({ 'string', 'number' }, type(lvl))) and 'info' or lvl
-
-    opts = (opts ~= nil and type(opts) == 'table') and opts or Notify.Opts
+    opts = opts or Notify.Opts
 
     if exists('notify') then
-        local notify = require('notify')
-
         if type(lvl) == 'number' then
             lvl = math.floor(lvl)
             lvl = (lvl <= OFF and lvl >= TRACE) and Notify.Levels[lvl] or Notify.Levels[INFO]
@@ -105,10 +95,6 @@ function Notify.notify(msg, lvl, opts)
         end
 
         opts = vim.tbl_deep_extend('keep', opts, Notify.Opts)
-
-        vim.schedule(function()
-            notify(msg, lvl, opts)
-        end)
     else
         if type(lvl) == 'string' then
             if vim.tbl_contains(Notify.Levels, string.lower(lvl)) then
@@ -121,31 +107,31 @@ function Notify.notify(msg, lvl, opts)
         else
             lvl = INFO
         end
-
-        vim.schedule(function()
-            vim.notify(msg, lvl, opts)
-        end)
     end
+
+    local fn = vim.schedule_wrap(function()
+        vim.notify(msg, lvl, opts)
+    end)
+    fn()
+
+    return fn
 end
 
 ---@param lvl VimNotifyLvl
 ---@return fun(args: vim.api.keyset.create_user_command.command_args)
 local function gen_cmd_lvl(lvl)
-    return function(args)
+    return function(args) ---@param args vim.api.keyset.create_user_command.command_args
         local notify = Notify.notify
         local data = args.args
-
         if data == '' then
             return
         end
-
         local opts = {
             animate = true,
             title = 'Message',
             timeout = 1750,
             hide_from_history = args.bang,
         }
-
         notify(data, lvl, opts)
     end
 end
@@ -172,5 +158,4 @@ vim.api.nvim_create_user_command('NotifyError', gen_cmd_lvl(vim.log.levels.ERROR
 })
 
 return Notify
-
 --- vim:ts=4:sts=4:sw=4:et:ai:si:sta:
