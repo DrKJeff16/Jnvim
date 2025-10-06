2e4a6a61e041f895e79731505265f60f35f0b0cc
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-10-06 17:00:28 -0600

    fdx(ts): tightened atocommand
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/ts.lua
+++ lua/plugin/ts.lua
@@ -1,15 +1,3 @@
-local User = require('user_api')
-local Check = User.check
-
-local Keymaps = User.config.keymaps
-local desc = User.maps.desc
-local exists = Check.exists.module
-if not exists('nvim-treesitter') then
-    return
-end
-
-local TS = require('nvim-treesitter')
-local ts_repeat_move = require('nvim-treesitter.textobjects.repeatable_move')
 local ensure_langs = {
     'bash',
     'c',
@@ -45,50 +33,62 @@ local ensure_langs = {
     'yaml',
 }
 
-TS.setup({
-    ---Directory to install parsers and queries to
-    install_dir = vim.fn.stdpath('data') .. '/site',
-})
-
-TS.install(ensure_langs):wait(300000)
-
+require('nvim-treesitter').setup({ install_dir = vim.fn.stdpath('data') .. '/site' })
+require('nvim-treesitter').install(ensure_langs):wait(300000)
 vim.api.nvim_create_autocmd('FileType', {
-    pattern = '*',
+    pattern = {
+        'asm',
+        'bash',
+        'c',
+        'cmake',
+        'codeowners',
+        'conf',
+        'config',
+        'confini',
+        'cpp',
+        'css',
+        'diff',
+        'dockerfile',
+        'dosini',
+        'doxygen',
+        'gitattributes',
+        'gitcommit',
+        'gitcommit',
+        'gitconfig',
+        'gitignore',
+        'gitrebase',
+        'gpg',
+        'html',
+        'hyprlang',
+        'java',
+        'json',
+        'json5',
+        'jsonc',
+        'julia',
+        'lisp',
+        'llvm',
+        'log',
+        'lsp_markdown',
+        'lua',
+        'make',
+        'markdown',
+        'python',
+        'query',
+        'readline',
+        'regex',
+        'sh',
+        'ssh_config',
+        'toml',
+        'udev',
+        'vim',
+        'vimdoc',
+        'yaml',
+        'zsh',
+    },
     callback = function(ev)
         vim.treesitter.start(ev.buf)
         vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
         vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
     end,
 })
-
----@type AllMaps
-local Keys = {
-    [';'] = {
-        ts_repeat_move.repeat_last_move,
-        desc('Repeat Last Move (Next)'),
-    },
-    [','] = {
-        ts_repeat_move.repeat_last_move_opposite,
-        desc('Repeat Last Move (Previous)'),
-    },
-    ['f'] = {
-        ts_repeat_move.builtin_f_expr,
-        desc('Builtin `f` Expr', true, nil, true, false, true),
-    },
-    ['F'] = {
-        ts_repeat_move.builtin_F_expr,
-        desc('Builtin `F` Expr', true, nil, true, false, true),
-    },
-    ['t'] = {
-        ts_repeat_move.builtin_t_expr,
-        desc('Builtin `t` Expr', true, nil, true, false, true),
-    },
-    ['T'] = {
-        ts_repeat_move.builtin_T_expr,
-        desc('Builtin `T` Expr', true, nil, true, false, true),
-    },
-}
-
-Keymaps({ n = Keys, o = Keys, x = Keys })
-
 --- vim:ts=4:sts=4:sw=4:et:ai:si:sta:
