f9e175cf4c01e4e02dd1bc806a48834ec05326a8
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-10-16 14:34:20 -0600

    chore(mini): fixed mini keymaps
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/plugin/_spec/essentials.lua
+++ lua/plugin/_spec/essentials.lua
@@ -7,7 +7,9 @@ return {
     {
         'nvim-mini/mini.nvim',
         version = false,
-        config = require('config.util').require('plugin.mini'),
+        config = function()
+            require('user_api.config').keymaps({ n = { ['<leader>m'] = { group = '+Mini' } } })
+        end,
     },
 }
 --- vim:ts=4:sts=4:sw=4:et:ai:si:sta:
--- lua/plugin/mini.lua
+++
@@ -1,2 +0,0 @@
-require('user_api.config').keymaps({ n = { ['<leader>m'] = { group = '+Mini' } } })
---- vim:ts=4:sts=4:sw=4:et:ai:si:sta:
